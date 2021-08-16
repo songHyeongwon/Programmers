00688d60ae7c44543a6db4da8feb2d2369dbf193
iks16 <iks16@DESKTOP-IT7VR6H>
2021-08-16 20:25:05 +0900

    2차 커밋

---
+++ MyProject/src/lavel1/Solution2.java
@@ -0,0 +1,49 @@
+package lavel1;
+import java.util.ArrayList;
+class Solution2 {
+    public int solution(int[][] board, int[] moves) {
+        int[][] newBoard = new int[board.length][board.length];
+		
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		
+		for(int i = 0; i<board.length ; i++) {
+			for(int k = 0; k<board.length ; k++) {
+				newBoard[k][i] = board[i][k];
+			}
+		}
+		
+		for(int i =0; i<moves.length; i++) {
+			int count = 0;
+			while (true) {
+				if(count == board.length) {
+					break;
+				}
+				if(newBoard[moves[i]-1][count] > 0) {
+					arr.add(newBoard[moves[i]-1][count]);
+					newBoard[moves[i]-1][count] = 0;
+					break;
+				}
+				count++;
+			}
+		}
+		//[4, 3, 1, 3, 4, 2, 4, 4]
+		int k = arr.size();
+		getAnswer(arr, 0);
+		int j = arr.size();
+        return k-j;
+    }
+    
+    public static int getAnswer(ArrayList<Integer> arr, int result) {
+		if(arr.size() == 0) return 0;
+		
+		for(int i = 0; i<arr.size()-1; i++) {
+			if(arr.get(i) == arr.get(i+1)) {
+				result = 1;
+				arr.remove(i+1);
+				arr.remove(i);
+				result += getAnswer(arr , result);
+			}
+		}
+		return result;
+	}
+}
