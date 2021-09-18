404aaa278da1ac2446a650b14e53102a600a0876
iks1681@naver.com <iks16@DESKTOP-IT7VR6H>
2021-09-18 22:09:12 +0900

    카카오 2021신입공채 2차 문제의 기본틀만 구현해보았다

--- KaKao/src/main/Main.java
+++ KaKao/src/main/Main.java
@@ -9,29 +9,76 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.xml.crypto.Data;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Main {
-	String HOST_URL = "https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users";
-	String token = "bd3c0c5b9600175d3bb4bc60b3d4da81";
+	//메인 설정
+	final String HOST_URL = "https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users";
+	final String token = "bd3c0c5b9600175d3bb4bc60b3d4da81";
+	String auth_key = "";
+	
 	
 	public static void main(String[] args) {
 		Main main = new Main();
+		JSONObject startResult = main.startApi(1);
+		main.auth_key = startResult.getString("auth_key");
+				
+		JSONObject bikesMap = main.actionAPI("locations", "GET", null);
+		JSONObject trucksMap = main.actionAPI("trucks", "GET", null);
 		
-		System.out.println(main.actionAPI(null).toString());
+		/*while(true) {
+			
+		}*/
+		JSONObject commands = new JSONObject();
+		JSONArray jsonArr = new JSONArray();
+		JSONObject obj = new JSONObject();
+		obj.put("truck_id", 0);
+		obj.put("command", new int[] {2,5,4,6,1});
+		
+		jsonArr.put(obj);
+		commands.put("commands", jsonArr);
+		JSONObject resultMap = main.actionAPI("simulate" , "PUT", commands);
+		
+		JSONObject scoreMap = main.actionAPI("score", "GET", null);
 	}
 	
-	public JSONObject actionAPI(JSONArray commandlist) {
+    /*public static void main(String[] args) {
+        String token = "YOUR_ACCESS_TOKEN";// 네아로 접근 토큰 값";
+        String header = "Bearer " + token; // Bearer 다음에 공백 추가
+        try {
+            String apiURL = "https://openapi.naver.com/v1/nid/me";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Authorization", header);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }*/
+	
+	public JSONObject startApi(int problem) {
 	    HttpURLConnection conn = null;
 	    JSONObject responseJson = null;
 	    
 	    try {
 	        //URL 설정
-	        URL url = new URL(this.HOST_URL + "/action");
+	        URL url = new URL(this.HOST_URL + "/start");
 
 	        conn = (HttpURLConnection) url.openConnection();
 	        //Request 형식 설정
@@ -44,7 +91,7 @@ public class Main {
 	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
 	        //commands라는 JSONArray를 담을 JSONObject 생성
 	        JSONObject commands = new JSONObject();
-	        commands.put("commands", commandlist);
+	        commands.put("problem", problem);
 	        //request에 쓰기
 	        bw.write(commands.toString());
 	        bw.flush();
@@ -77,6 +124,57 @@ public class Main {
 	        System.out.println("not JSON Format response");
 	        e.printStackTrace();
 	    }
+	    System.out.println(responseJson.toString());
+	    return responseJson;
+	}
+	public JSONObject actionAPI(String apiType, String method, JSONObject commands) {
+	    HttpURLConnection conn = null;
+	    JSONObject responseJson = null;
+	    
+	    try {
+	        //URL 설정
+	        URL url = new URL(this.HOST_URL + "/" + apiType);
+
+	        conn = (HttpURLConnection) url.openConnection();
+	        //Request 형식 설정
+	        conn.setRequestMethod(method);
+	        
+	        conn.setRequestProperty("Authorization", this.auth_key);
+	        conn.setRequestProperty("Content-Type", "application/json");
+	        
+	        if(commands != null) {
+	        	//request에 JSON data 준비
+		        conn.setDoOutput(true);
+		        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+		        //request에 쓰기
+		        bw.write(commands.toString());
+		        bw.flush();
+		        bw.close();
+	        }
+	        
+	        //보내고 결과값 받기
+	        int responseCode = conn.getResponseCode();
+	        if (responseCode != 200) {
+	        	System.out.println(responseCode + " / " + conn.getResponseMessage());
+	        } else { // 성공 후 응답 JSON 데이터받기
+	            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	            StringBuilder sb = new StringBuilder();
+	            String line = "";
+	            while ((line = br.readLine()) != null) {
+	                sb.append(line);
+	            }
+	            responseJson = new JSONObject(sb.toString());
+	            
+	        }
+	    } catch (MalformedURLException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } catch (JSONException e) {
+	        System.out.println("not JSON Format response");
+	        e.printStackTrace();
+	    }
+	    System.out.println(responseJson.toString());
 	    return responseJson;
 	}
 }
