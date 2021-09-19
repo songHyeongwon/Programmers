fd04c54cd4b11b801d02d6bd36872c9fd4b457d6
iks1681@naver.com <iks16@DESKTOP-IT7VR6H>
2021-09-19 19:38:04 +0900

    필요 주석 추가 n명이 입국심사를 위해 줄을 서서 기다리고 있습니다. 각 입국심사대에 있는 심사관마다 심사하는데 걸리는 시간은 다릅니다.
    처음에 모든 심사대는 비어있습니다. 한 심사대에서는 동시에 한 명만 심사를 할 수 있습니다. 가장 앞에 서 있는 사람은 비어 있는
    심사대로 가서 심사를 받을 수 있습니다. 하지만 더 빨리 끝나는 심사대가 있으면 기다렸다가 그곳으로 가서 심사를 받을 수도
    있습니다.
    
    모든 사람이 심사를 받는데 걸리는 시간을 최소로 하고 싶습니다.
    
    입국심사를 기다리는 사람 수 n, 각 심사관이 한 명을 심사하는데 걸리는 시간이 담긴 배열 times가 매개변수로 주어질 때,
    모든 사람이 심사를 받는데 걸리는 시간의 최솟값을 return 하도록 solution 함수를 작성해주세요.

--- MyProject/src/lavel3/Solution14.java
+++ MyProject/src/lavel3/Solution14.java
@@ -8,25 +8,23 @@ public class Solution14 {
 		
 		System.out.println(new Solution14().solution(6, times));
 	}
-	
+	//이진탐색
 	public long solution(int n, int[] times) {
         Arrays.sort(times);
         long min = times[0];
 		long max = (long) n * times[times.length-1];
         long answer = max;
         
-        while(min <= max) {
+        while(min <= max) {//최소가 최대보다 작을동안만
         	long mid = (max + min)/2;
-        	
         	long check = isCheck(mid , times);
-        	
         	if(check < n) {
         		//시간 늘이기
-            	min = mid + 1;
+            	min = mid + 1; //(max + min)/2 +1;
         	} else {
         		//시간 줄이기
-        		max = mid - 1;
-        		answer = Math.min(mid, answer);
+        		max = mid - 1; //(max + min)/2 -1;
+        		answer = Math.min(mid, answer); //최소값으로 항상 갱신
         	}
         }
         return answer;
