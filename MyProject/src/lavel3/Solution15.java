dd2e0678937ff7584199b32599f7bffaf85ef962
iks1681@naver.com <iks16@DESKTOP-IT7VR6H>
2021-09-20 14:03:16 +0900

    아래와 같이 5와 사칙연산만으로 12를 표현할 수 있습니다.
    12 = 5 + 5 + (5 / 5) + (5 / 5)
    12 = 55 / 5 + 5 / 5
    12 = (55 + 5) / 5
    
    5를 사용한 횟수는 각각 6,5,4 입니다. 그리고 이중 가장 작은 경우는 4입니다.
    이처럼 숫자 N과 number가 주어질 때, N과 사칙연산만 사용해서 표현 할 수 있는 방법 중 N 사용횟수의 최솟값을
    return 하도록 solution 함수를 작성하세요.

---
+++ MyProject/src/lavel3/Solution15.java
@@ -0,0 +1,43 @@
+package lavel3;
+//https://programmers.co.kr/learn/courses/30/lessons/42895?language=java
+public class Solution15 {
+	public static void main(String[] args) {
+		
+		System.out.println(new Solution15().solution(0, 0));
+	}
+	
+	//출처 : https://velog.io/@jwkim/DFS-n-expression
+	private int n;
+	private int target;
+	private int answer = Integer.MAX_VALUE;
+	
+	public int solution(int N, int number) {
+		n = N;
+		target = number;
+		dfs(0, 0);// 연산횟수 , 결과값
+        return answer == Integer.MAX_VALUE ? -1 : answer; //답이 변경되었으면 답 아니면 -1
+    }
+
+	private void dfs(int count, int prev) {
+		// TODO Auto-generated method stub
+		if(count > 8) {
+			answer = -1;
+			return;
+		}
+		
+		if(prev == target) {
+			answer = Math.min(count, answer);
+			return;
+		}
+		
+		int tempN = n;
+		for(int i = 0; i < 8-count; i++) {
+			int count2 = count+ i + 1;
+			dfs(count2, prev + tempN);
+			dfs(count2, prev - tempN);
+			dfs(count2, prev / tempN);
+			dfs(count2, prev * tempN);
+			tempN = tempN * 10 + n;//템프의 단위를 10단위 상승
+		}
+	}
+}
