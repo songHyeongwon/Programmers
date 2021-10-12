637c458a0a0e7183d7b822e63192dead28fafe79
iks1681@naver.com <iks16@DESKTOP-IT7VR6H>
2021-10-12 19:01:18 +0900

    백준 7일차

---
+++ BaekJoon/src/step10/Main10870.java
@@ -0,0 +1,20 @@
+package step10;
+
+import java.util.Scanner;
+
+public class Main10870 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(getPi(0, 1, 2 ,n));
+		sc.close();
+	}
+
+	private static int getPi(int a, int b, int cnt, int n) {
+		// TODO Auto-generated method stub
+		if(n == 0) return 0;
+		if(n == 1) return 1;
+		if(cnt == n) return a+b;
+		return getPi(b , a+b, cnt+1, n);
+	}
+}
