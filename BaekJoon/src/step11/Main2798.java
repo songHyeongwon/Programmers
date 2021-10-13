f9705072e3bc9aa320948a7c8b8a9aefe3d597a1
iks1681@naver.com <iks16@DESKTOP-IT7VR6H>
2021-10-13 21:16:05 +0900

    백준 8일차

---
+++ BaekJoon/src/step11/Main2798.java
@@ -0,0 +1,44 @@
+package step11;
+
+import java.util.Scanner;
+
+public class Main2798 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int sum = 0;//гу╟Х
+		
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sum = getSum(arr , n ,m);
+		
+		System.out.println(sum);
+		
+		sc.close();
+	}
+
+	static int getSum(int[] arr, int n, int m) {
+		// TODO Auto-generated method stub
+		int result = 0;
+		
+		for (int i = 0; i < n-2; i++) {
+			for (int j = i+1; j < n-1; j++) {
+				for (int k = j+1; k < n; k++) {
+					int tempSum = arr[i] + arr[j] + arr[k];
+					if(tempSum == m) return tempSum;
+					if(tempSum < m) {
+						result = Math.max(result, tempSum);
+					}
+				}
+			}
+		}
+		
+		return result;
+	}
+}
