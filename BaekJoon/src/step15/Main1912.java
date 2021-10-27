cb6c415694c0240b6d3885766e861039622c7efc
songHyeongwon <iks1681@naver.com>
2021-10-27 20:42:15 +0900

    백준 DP문제 계속 풀고 있음

---
+++ BaekJoon/src/step15/Main1912.java
@@ -0,0 +1,31 @@
+package step15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main1912 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+		StringTokenizer token = new StringTokenizer(br.readLine() , " ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(token.nextToken());
+		}
+		dp[0] = arr[0];
+		int max = arr[0];
+		
+		for (int i = 1; i < arr.length; i++) {
+			dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+			max = Math.max(max, dp[i]);
+		}
+		//System.out.println(Arrays.toString(dp));
+		
+		System.out.println(max);
+	}
+}
---
+++ BaekJoon/src/step15/Main9095.java
@@ -0,0 +1,22 @@
+package step15;
+
+import java.util.Scanner;
+
+public class Main9095 {
+	static int[] dp = null;
+	public static void main(String[] args) {
+		dp = new int[12];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for(int i = 4; i <= 11; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		}
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			System.out.println(dp[sc.nextInt()]);
+		}
+		sc.close();
+	}
+}
