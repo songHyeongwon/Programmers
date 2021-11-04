d5143f61156205d8184df89d9c651c3614dae73d
songHyeongwon <iks1681@naver.com>
2021-11-04 20:36:32 +0900

    드디어 DP를 다 풀었단다
    Signed-off-by: songHyeongwon <iks1681@naver.com>

---
+++ BaekJoon/src/step15/Main9251.java
@@ -0,0 +1,25 @@
+package step15;
+
+import java.util.Scanner;
+
+public class Main9251 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		String b = sc.nextLine();
+		sc.close();
+		
+		int[][] dp = new int[a.length() + 1][b.length() + 1];
+		
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 1; j < dp[i].length; j++) {
+				if(a.charAt(i-1) == b.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] +1;
+				} else {
+					dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+				}
+			}
+		}
+		System.out.println(dp[a.length()][b.length()]);
+	}
+}
