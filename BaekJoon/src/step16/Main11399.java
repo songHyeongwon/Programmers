2c5ca10070d092fb1ac6005bf1c618883efecdd3
songHyeongwon <iks1681@naver.com>
2021-11-09 20:32:12 +0900

    백준 그리디 알고리즘

---
+++ BaekJoon/src/step16/Main11399.java
@@ -0,0 +1,25 @@
+package step16;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main11399 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+		for (int i = 0; i < N; i++) {
+			queue.add(sc.nextInt());
+		}
+		int result = 0;
+		int befor = 0;
+		while(!queue.isEmpty()) {
+			int temp = queue.poll();
+			temp += befor;
+			result += temp;
+			befor = temp;
+		}
+		System.out.println(result);
+		sc.close();
+	}
+}
