5506f15bdfb1951c00cf8984356f29427c5fb65d
songHyeongwon <iks1681@naver.com>
2021-11-19 21:04:32 +0900

    최대공약수 구하기에 자주사용될 함수 	private static int gcd(int a, int b) { 		while(b != 0) { 			int r =a % b; 			a=b; 			b=r; 		} 		return a; 	}
    Signed-off-by: songHyeongwon <iks1681@naver.com>

---
+++ BaekJoon/src/step17/Main3036.java
@@ -0,0 +1,29 @@
+package step17;
+
+import java.util.Scanner;
+
+public class Main3036 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int rollCnt = sc.nextInt();
+		for (int i = 1; i < N; i++) {
+			int temp = sc.nextInt();
+			int gcd = gcd(temp , rollCnt);
+			System.out.print(rollCnt / gcd);
+			System.out.print("/");
+			System.out.println(temp / gcd);
+		}
+		
+		sc.close();
+	}
+	//최대공약수
+	private static int gcd(int a, int b) {
+		while(b != 0) {
+			int r =a % b;
+			a=b;
+			b=r;
+		}
+		return a;
+	}
+}
