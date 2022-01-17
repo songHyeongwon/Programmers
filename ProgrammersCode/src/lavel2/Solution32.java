528a4afbe0de0779a99aa371417f1afe6dad7292
songHyeongwon <iks1681@naver.com>
2022-01-17 19:46:25 +0900

    카카오 블라인드 코테 2

---
+++ ProgrammersCode/src/lavel2/Solution32.java
@@ -0,0 +1,46 @@
+package lavel2;
+
+//https://programmers.co.kr/learn/courses/30/lessons/92335?language=java
+public class Solution32 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 1000000 ; i++) {
+			for(int j = 3; j <= 10; j++) {
+				System.out.println(i+ " / "+j);
+				System.out.println(new Solution32().solution(i , j));
+			}
+		}
+	}
+	
+	public int solution(int n, int k) {
+        int answer = 0;
+        
+        String[] kWlstn = getKString(n, k).split("0");
+        for(int i = 0; i< kWlstn.length; i++) {
+        	if(!kWlstn[i].equals("") && isThtn(kWlstn[i])) {
+        		answer++;
+        	}
+        }
+        
+        return answer;
+    }
+	
+	private boolean isThtn(String string) {
+		long checkNum = Long.parseLong(string);
+		if(checkNum == 1) return false;
+		
+		for(int i = 2; i <= Math.sqrt(checkNum); i++) {
+			if(checkNum % i == 0) return false;
+		}
+		
+		return true;
+	}
+	public String getKString (int n , int k) {
+		String ans ="";
+		
+		while(n>0) {
+			ans = (n % k) + ans;
+			n /= k;
+		}
+		return ans;
+	}
+}
