3755bf8133e7b2715ec1bd55fd97f81188092d79
songHyeongwon <iks1681@naver.com>
2022-01-26 20:17:52 +0900

    어휴 피곤했는데 1단계문제 마침 잘됐다 ㅠㅠ

---
+++ BaekJoon/src/step01/Main10926.java
@@ -0,0 +1,11 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Main10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine() + "??!");
+		sc.close();
+	}
+}
