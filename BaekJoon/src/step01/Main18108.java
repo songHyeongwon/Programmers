40996b3ea42d31067ce68fc2f31955c6aad92394
songHyeongwon <iks1681@naver.com>
2022-01-27 21:54:33 +0900

    흐허허헣 지금 퇴근했으니.. 오늘은 이것만 커밋

---
+++ BaekJoon/src/step01/Main18108.java
@@ -0,0 +1,12 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Main18108 {
+	public static void main(String[] args) {
+		int yranTem = 543;
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() - yranTem);
+		sc.close();
+	}
+}
