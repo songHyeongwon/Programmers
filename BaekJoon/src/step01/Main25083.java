b1fa8221a9af5fa97dadd217dacabba4823d95d3
songHyeongwon <iks1681@naver.com>
2022-05-15 00:01:53 +0900

    백준 1문제 푸쉬

---
+++ BaekJoon/src/step01/Main25083.java
@@ -0,0 +1,14 @@
+package step01;
+
+public class Main25083 {
+	public static void main(String[] args) {
+		String out = ""
+				+ "         ,r'\"7"
+				+ "\nr`-_   ,'  ,/"
+				+ "\n \\. \". L_r'"
+				+ "\n   `~\\/"
+				+ "\n      |"
+				+ "\n      |";
+		System.out.println(out);
+	}
+}
