a2919d73b63a178b52530439396f52047c30dbd3
songHyeongwon <iks1681@naver.com>
2022-05-29 12:37:53 +0900

    백준 챕터 수정

---
+++ BaekJoon/src/step06/Main1152.java
@@ -0,0 +1,20 @@
+package step06;
+
+import java.util.Scanner;
+
+public class Main1152 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		
+		String[] aArr = a.trim().split(" ");
+		
+		if(aArr[0].equals("")) {
+			System.out.println(0);
+		} else {
+			System.out.println(aArr.length);
+		}
+		
+		sc.close();
+	}
+}
---
+++ BaekJoon/src/step06/Main2675.java
@@ -0,0 +1,25 @@
+package step06;
+
+import java.util.Scanner;
+
+public class Main2675 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int i = 0; i < T; i++) {
+			int R = sc.nextInt();
+			String S = sc.next();
+			StringBuffer buf = new StringBuffer();
+			
+			for (int j = 0; j < S.length(); j++) {
+				for (int j2 = 0; j2 < R; j2++) {
+					buf.append(S.charAt(j));
+				}
+			}
+			System.out.println(buf.toString());
+		}
+		sc.close();
+	}
+}
