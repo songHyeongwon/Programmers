bb1cb0e0950f36b2d891afc8bedab9e2930c6b67
songHyeongwon <iks1681@naver.com>
2022-06-02 20:44:44 +0900

    백준 챕터 수정

---
+++ BaekJoon/src/step09/Main10872.java
@@ -0,0 +1,17 @@
+package step09;
+
+import java.util.Scanner;
+
+public class Main10872 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(getPack(n, 1));
+		sc.close();
+	}
+
+	private static int getPack(int n, int temp) {
+		if(n == 0) return temp;
+		return getPack(n-1 , temp *= n);
+	}
+}
