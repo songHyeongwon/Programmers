fe1f04e1a6ed1659c6564ef4390aab0fe79a476a
songHyeongwon <iks1681@naver.com>
2022-06-04 00:22:00 +0900

    백준 위치 변경

---
+++ BaekJoon/src/step10/Main1018.java
@@ -0,0 +1,47 @@
+package step10;
+
+import java.util.Scanner;
+
+public class Main1018 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		boolean[][] arr = new boolean[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			String a = sc.next();
+			for (int j = 0; j < M; j++) {
+				if(a.charAt(j) == 'W') {
+					arr[i][j] = true;
+				}
+			}
+
+		}
+		sc.close();
+		int minCnt = 8*8;
+		for (int i = 0; i <= N-8; i++) {
+			for (int j = 0; j <= M-8; j++) {
+				minCnt = Math.min(minCnt, getCount(arr, i, j));
+			}
+		}
+		System.out.println(minCnt);
+	}
+
+	private static int getCount(boolean[][] arr, int y, int x) {
+		int result = 0;
+		boolean temp = arr[y][x];
+		for (int i = 0; i < 8; i++) {
+			temp = !temp;
+			for (int j = 0; j < 8; j++) {
+				temp = !temp;
+				if(i == 0 && j == 0) continue;
+				if(arr[y+i][x+j] != temp) {
+					result++;
+				}
+			}
+		}
+		return Math.min(result, 64-result);
+	}
+}
