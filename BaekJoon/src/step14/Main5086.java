8e49aca17f0e9eeed99faa83028d76804b861076
songHyeongwon <iks1681@naver.com>
2022-07-19 21:42:15 +0900

    14장 푸쉬

---
+++ BaekJoon/src/step14/Main5086.java
@@ -0,0 +1,24 @@
+package step14;
+
+import java.util.Scanner;
+
+public class Main5086 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a+b ==0) break;
+			
+			String result = "neither";
+			if(a % b == 0) {
+				result = "multiple";
+			}
+			if(b % a == 0) {
+				result = "factor";
+			}
+			System.out.println(result);
+		}
+		sc.close();
+	}
+}
