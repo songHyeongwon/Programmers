4a016e0df9f33a0c2ed0f3de543cbd49a114e3df
songHyeongwon <iks1681@naver.com>
2022-07-24 00:01:35 +0900

    19장 푸쉬

---
+++ BaekJoon/src/step19/Main10828.java
@@ -0,0 +1,53 @@
+package step19;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main10828 {
+	
+	public static void main(String[] args) throws Exception{
+		Stack<Integer> stack = new Stack<Integer>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tk = null;
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			tk = new StringTokenizer(str , " ");
+			switch (tk.nextToken()) {
+			case "push":
+				stack.add(Integer.parseInt(tk.nextToken()));
+				break;
+			case "pop":
+				if(stack.isEmpty()) {
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(stack.pop()).append("\n");
+				}
+				break;
+			case "size":
+				sb.append(stack.size()).append("\n");
+				break;
+			case "empty":
+				if(stack.isEmpty()) {
+					sb.append(1).append("\n");
+				} else {
+					sb.append(0).append("\n");
+				}
+				break;
+			case "top":
+				if(stack.isEmpty()) {
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(stack.peek()).append("\n");
+				}
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+}
