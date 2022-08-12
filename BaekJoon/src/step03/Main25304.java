8277f9f9ff3e0af093101423e7baa761c9795e05
songHyeongwon <iks1681@naver.com>
2022-08-12 22:31:35 +0900

    백준 새로 생긴 3단계 문제 푸쉬

---
+++ BaekJoon/src/step03/Main25304.java
@@ -0,0 +1,19 @@
+package step03;
+
+import java.util.Scanner;
+
+public class Main25304 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			x -= a*b;
+		}
+		sc.close();
+		if(x==0) System.out.println("Yes");
+		else System.out.println("No");
+	}
+}
