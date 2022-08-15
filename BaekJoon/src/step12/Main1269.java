9f65081d6e8de4d6f5efc82e894880aa57d38fff
songHyeongwon <iks1681@naver.com>
2022-08-16 00:10:31 +0900

    백준 1269 푸쉬

---
+++ BaekJoon/src/step12/Main1269.java
@@ -0,0 +1,27 @@
+package step12;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main1269 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < a + b; i++) {
+			int t = sc.nextInt();
+			if(map.containsKey(t)) {
+				map.put(t, map.get(t) + 1);
+			} else {
+				map.put(t, 1);
+			}
+		}
+		int count = 0;
+		for(Integer t : map.keySet()) {
+			if(map.get(t) == 1) count++;
+		}
+		System.out.println(count);
+		sc.close();
+	}
+}
