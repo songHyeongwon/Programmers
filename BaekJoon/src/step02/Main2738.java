03533662ae3efe874425afca16d8872ebd244acd
songHyeongwon <iks1681@naver.com>
2023-08-29 19:48:47 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step02/Main2738.java

---
+++ BaekJoon/src/step02/Main2738.java
@@ -0,0 +1,28 @@
+package step02;
+
+import java.util.Scanner;
+
+public class Main2738 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] arr = new int[n][m];
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + sc.nextInt()+ " ");
+			}
+			System.out.println();
+		}
+		
+		
+		sc.close();
+	}
+}
