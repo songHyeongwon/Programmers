d9470c98d22d3d0cbfd8e9129081894d89b4f1e5
songHyeongwon <iks1681@naver.com>
2023-09-11 20:05:51 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step04/Main2525.java

---
+++ BaekJoon/src/step04/Main2525.java
@@ -0,0 +1,17 @@
+package step04;
+
+import java.util.Scanner;
+
+public class Main2525 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int m = sc.nextInt();
+		int next = sc.nextInt();
+		sc.close();
+		int temp = H * 60 + m + next;
+		int nextM = temp%60;
+		int nextH = (temp - temp%60) / 60;
+		System.out.println(nextH%24 + " " + nextM);
+	}
+}
