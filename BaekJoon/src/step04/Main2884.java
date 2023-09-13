275e3ad1231d629b1f34f6d9c9444f1db75e5253
songHyeongwon <iks1681@naver.com>
2023-09-13 20:56:23 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step04/Main2884.java

---
+++ BaekJoon/src/step04/Main2884.java
@@ -0,0 +1,21 @@
+package step04;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main2884 {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		Date date = new Date();
+		
+		date.setHours(a);
+		date.setMinutes(b-45);
+
+		System.out.println(date.getHours() +" " +date.getMinutes());
+		sc.close();
+	}
+}
