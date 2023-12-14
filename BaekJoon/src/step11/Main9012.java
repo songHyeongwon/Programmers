eaa51614311650eb543aa53b231770ad4a2ee20a
songHyeongwon <iks1681@naver.com>
2023-12-15 08:29:42 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step11/Main9012.java

---
+++ BaekJoon/src/step11/Main9012.java
@@ -0,0 +1,34 @@
+package step11;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main9012 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < N; i++) {
+			System.out.println(result(sc.nextLine()));
+		}
+		sc.close();
+	}
+	
+	public static String result(String k) {
+		Stack<Character> statc = new Stack<Character>();
+		statc.add('(');
+		for (int i = 0; i < k.length(); i++) {
+			if(statc.isEmpty()) return "NO";
+			if(k.charAt(i) == '(') {
+				statc.add('(');
+			} else {
+				statc.pop();
+			}
+		}
+		if(statc.size() == 1 && statc.pop() == '(') {
+			return "YES";
+		} else {
+			return "NO";
+		}
+	}
+}
