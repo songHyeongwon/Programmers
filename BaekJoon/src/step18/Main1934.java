08e047d1979aec0581120f089396850d9a995386
songHyeongwon <iks1681@naver.com>
2024-02-07 19:46:39 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step18/Main1934.java

---
+++ BaekJoon/src/step18/Main1934.java
@@ -0,0 +1,27 @@
+package step18;
+
+import java.util.Scanner;
+
+public class Main1934 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(getAns(a,b));
+		}
+		sc.close();
+	}
+
+	private static int getAns(int a, int b) {
+		int max = Math.max(a, b);
+		int min = Math.min(a, b);
+		int cnt = 0;
+		while (true) {
+			if(min * (++cnt) % max == 0) {
+				return min * cnt;
+			}
+		}
+	}
+}
