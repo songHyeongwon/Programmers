42a46cc35d5b6acc383c73b12d187e1e4b5eaae9
songHyeongwon <iks1681@naver.com>
2024-03-18 19:54:12 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step22/Main2231.java

---
+++ BaekJoon/src/step22/Main2231.java
@@ -0,0 +1,29 @@
+package step22;
+
+import java.util.Scanner;
+
+public class Main2231 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int result = 0;
+		
+		for (int i = n-1; i > 0; i--) {
+			int temp = getTemp(i);
+			if(temp == n) {
+				result = i;
+			}
+		}
+		sc.close();
+		System.out.println(result);
+	}
+
+	private static int getTemp(int i) {
+		String s = i+"";
+		for (int j = 0; j < s.length(); j++) {
+			i += Character.getNumericValue(s.charAt(j));
+		}
+		return i;
+	}
+}
