95a9fcd68995630c51c3e2aa4c275d4ec0f68a29
songHyeongwon <iks1681@naver.com>
2024-07-19 20:05:47 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/OUTTER/Main1712.java

---
+++ BaekJoon/src/OUTTER/Main1712.java
@@ -0,0 +1,18 @@
+package OUTTER;
+
+import java.util.Scanner;
+
+public class Main1712 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if(b >= c) System.out.println(-1);
+		else {
+			System.out.println(a / (c-b) +1);
+		}
+		sc.close();
+	}
+}
