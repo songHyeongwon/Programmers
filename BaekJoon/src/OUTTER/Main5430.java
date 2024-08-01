3042656158419fc1c647519eed1f3f18ca0cfd0d
songHyeongwon <iks1681@naver.com>
2024-08-01 12:39:47 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/OUTTER/Main5430.java

---
+++ BaekJoon/src/OUTTER/Main5430.java
@@ -0,0 +1,66 @@
+package OUTTER;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main5430 {
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		ArrayDeque<Integer> deque;
+		
+		for (int i = 0; i < T; i++) {
+			String fun = sc.next();
+			int N = sc.nextInt();
+			String arrText = sc.next();
+			String[] arr = arrText.substring(1, arrText.length() -1).split(",");
+			//System.out.println(fun + " " + N + " " + Arrays.toString(arr));
+			deque = new ArrayDeque<Integer>();
+			// 덱에 배열 원소를 넣어준다.
+			for(int j = 0; j < N; j++) {
+				deque.add(Integer.parseInt(arr[j]));
+			}
+			fun(fun , deque);
+		}
+		System.out.println(sb.toString());
+		sc.close();
+	}
+	public static void fun(String temp , ArrayDeque<Integer> list) {
+		boolean isFirst = true;
+		for (int i = 0; i < temp.length(); i++) {
+			if(temp.charAt(i) == 'R') {
+				isFirst = !isFirst;
+			} else {
+				if(list.size() == 0) {
+					sb.append("error\n");
+					return;
+				}
+				if(isFirst) {
+					list.pollFirst();
+				} else {
+					list.pollLast();
+				}
+			}
+		}
+		makeSb(isFirst , list);
+	}
+	private static void makeSb(boolean isFirst, ArrayDeque<Integer> list) {
+		sb.append("[");
+		if(list.size() > 0) {
+			if(isFirst) {
+				sb.append(list.pollFirst());
+				while(!list.isEmpty()) {
+					sb.append(",").append(list.pollFirst());
+				}
+			} else {
+				sb.append(list.pollLast());
+				while(!list.isEmpty()) {
+					sb.append(",").append(list.pollLast());
+				}
+			}
+		}
+		sb.append("]\n");
+	}
+}
