da96df5a5182996937782d989e9f44bc57940191
songHyeongwon <iks1681@naver.com>
2024-08-03 19:45:53 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/OUTTER/Main8958.java

---
+++ BaekJoon/src/OUTTER/Main8958.java
@@ -0,0 +1,26 @@
+package OUTTER;
+
+import java.util.Scanner;
+
+public class Main8958 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String temp = sc.next();
+			int point = 0;
+			int oCnt = 0;
+			for (int j = 0; j < temp.length(); j++) {
+				if(temp.charAt(j) == 'O') {
+					oCnt++;
+					point += oCnt;
+				} else {
+					oCnt = 0;
+				}
+			}
+			System.out.println(point);
+		}
+		sc.close();
+	}
+}
