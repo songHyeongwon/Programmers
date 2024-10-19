63452d0aff3af129d96369e1206955b54d4423e1
songHyeongwon <iks1681@naver.com>
2024-10-19 11:20:42 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step01/Main11382.java

---
+++ BaekJoon/src/step01/Main11382.java
@@ -0,0 +1,14 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Main11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		sc.close();
+		System.out.println(a + b + c);
+	}
+}
