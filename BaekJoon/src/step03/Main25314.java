b30e5ac7ee5499284801d80d275e2074d7e1ee75
songHyeongwon <iks1681@naver.com>
2024-10-20 12:32:41 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step03/Main25314.java

---
+++ BaekJoon/src/step03/Main25314.java
@@ -0,0 +1,17 @@
+package step03;
+
+import java.util.Scanner;
+
+public class Main25314 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < n; i += 4) {
+			buf.append("long ");
+		}
+		buf.append("int");
+		System.out.println(buf.toString());
+		sc.close();
+	}
+}
