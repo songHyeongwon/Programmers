4b7ab209f0c67bb5e2c236b8dfe635ed346b0e88
songHyeongwon <iks1681@naver.com>
2024-10-24 19:52:45 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main2743.java

---
+++ BaekJoon/src/step07/Main2743.java
@@ -0,0 +1,12 @@
+package step07;
+
+import java.util.Scanner;
+
+public class Main2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String stg = sc.nextLine();
+		System.out.println(stg.length());
+		sc.close();
+	}
+}
