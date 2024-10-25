1d4b340dab28f0361a036fd231a342e6eacbfbde
songHyeongwon <iks1681@naver.com>
2024-10-25 18:48:54 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main9086.java

---
+++ BaekJoon/src/step07/Main9086.java
@@ -0,0 +1,17 @@
+package step07;
+
+import java.util.Scanner;
+
+public class Main9086 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int row = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < row; i++) {
+			String stg = sc.nextLine();
+			System.out.print(stg.charAt(0));
+			System.out.println(stg.charAt(stg.length()-1));
+		}
+		sc.close();
+	}
+}
