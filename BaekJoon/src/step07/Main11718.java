44c1c8b28a65451839db5b9c36a5cd7b6ae17dfa
songHyeongwon <iks1681@naver.com>
2024-10-26 11:22:05 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main11718.java

---
+++ BaekJoon/src/step07/Main11718.java
@@ -0,0 +1,23 @@
+package step07;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main11718 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String temp = br.readLine();
+        while (temp != null) {
+        	bw.write(temp);
+            bw.newLine();
+        	temp = br.readLine();
+		}
+        bw.flush();
+        bw.close();
+	}
+}
