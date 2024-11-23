358ea0330560b293651b4c7896929c1a044134a7
songHyeongwon <iks1681@naver.com>
2024-11-23 12:48:21 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step08/Main2903.java

---
+++ BaekJoon/src/step08/Main2903.java
@@ -0,0 +1,12 @@
+package step08;
+
+import java.util.Scanner;
+
+public class Main2903 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		System.out.println( (long) Math.pow(Math.pow(2, t) + 1, 2));		
+		sc.close();
+	}
+}
