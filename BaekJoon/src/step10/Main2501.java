102f621d7d9e09a83a15b062d0b4f160129a3613
songHyeongwon <iks1681@naver.com>
2024-11-25 19:36:24 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step10/Main2501.java

---
+++ BaekJoon/src/step10/Main2501.java
@@ -0,0 +1,27 @@
+package step10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main2501 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		List<Integer> list = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				list.add(i);
+			}
+		}
+
+		if (list.size() <= k - 1) {
+			System.out.println(0);
+		} else {
+			System.out.println(list.get(k - 1));
+		}
+
+		sc.close();
+	}
+}
