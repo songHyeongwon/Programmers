4ffade905dfbc8f60ebe5046d10d51351515caa3
songHyeongwon <iks1681@naver.com>
2024-12-27 11:36:14 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step09/Main2587.java

---
+++ BaekJoon/src/step09/Main2587.java
@@ -0,0 +1,23 @@
+package step09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main2587 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		int mid = 0;
+		int[] arr = new int[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		Arrays.sort(arr);
+		int avg = sum / 5;
+		mid = arr[2];
+		System.out.print(avg + "\n" + mid);
+	}
+}
