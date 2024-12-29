f73c78eade490a442362f7a1ffe8c93230bc254a
songHyeongwon <iks1681@naver.com>
2024-12-29 11:28:04 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/JavaUtilProject/src/utilPackage/CreateGitBat.java

--- JavaUtilProject/src/utilPackage/CreateGitBat.java
+++ JavaUtilProject/src/utilPackage/CreateGitBat.java
@@ -14,33 +14,12 @@ public class CreateGitBat {
 	private static DateTimeFormatter formatter = null;
 	public static void main(String[] args) throws Exception{
 		formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		startDate = LocalDate.parse("2024-11-05", formatter);
+		startDate = LocalDate.parse("2024-12-28", formatter);
 		String temp = "C:/Temp/workspace/Programmers/";
 		ArrayList<String> list = new ArrayList<String>();
-		list.add(temp + "JavaUtilProject/bin/utilPackage/BaekJoonProblemSync.class");
 		list.add(temp + "JavaUtilProject/bin/utilPackage/CreateGitBat.class");
-		list.add(temp + "JavaUtilProject/bin/utilPackage/Test1.class");
-		list.add(temp + "JavaUtilProject/src/utilPackage/BaekJoonProblemSync.java");
 		list.add(temp + "JavaUtilProject/src/utilPackage/CreateGitBat.java");
-		list.add(temp + "JavaUtilProject/src/utilPackage/Test1.java");
-		list.add(temp + "BaekJoon/bin/step02/Main10798.class");
-		list.add(temp + "BaekJoon/bin/step08/Main2720.class");
-		list.add(temp + "BaekJoon/bin/step08/Main2745.class");
-		list.add(temp + "BaekJoon/bin/step08/Main2903.class");
-		list.add(temp + "BaekJoon/bin/step08/Main11005.class");
-		list.add(temp + "BaekJoon/bin/step10/Main2501.class");
-		list.add(temp + "BaekJoon/bin/step10/Main9506.class");
-		list.add(temp + "BaekJoon/bin/step52/Main2444.class");
-		list.add(temp + "BaekJoon/bin/step52/Main25206.class");
-		list.add(temp + "BaekJoon/src/step02/Main10798.java");
-		list.add(temp + "BaekJoon/src/step08/Main2720.java");
-		list.add(temp + "BaekJoon/src/step08/Main2745.java");
-		list.add(temp + "BaekJoon/src/step08/Main2903.java");
-		list.add(temp + "BaekJoon/src/step08/Main11005.java");
-		list.add(temp + "BaekJoon/src/step10/Main2501.java");
-		list.add(temp + "BaekJoon/src/step10/Main9506.java");
-		list.add(temp + "BaekJoon/src/step52/Main2444.java");
-		list.add(temp + "BaekJoon/src/step52/Main25206.java");
+
 		for(String s : list) {
 			printFile(s);
 			startDate = startDate.plusDays(1);
