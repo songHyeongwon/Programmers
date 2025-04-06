c1f63628210a1fd6f84738e00891df0f02044d30
songHyeongwon <iks1681@naver.com>
2025-04-06 12:52:32 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step06/Main10811.java

--- BaekJoon/src/step06/Main10811.java
+++ BaekJoon/src/step06/Main10811.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main10811 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int[] arr = new int[n];
