3bc24ea62434e30324b821ffa4b8db15dc9d4203
songHyeongwon <iks1681@naver.com>
2025-05-14 19:44:58 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step08/Main11005.java

--- BaekJoon/src/step08/Main11005.java
+++ BaekJoon/src/step08/Main11005.java
@@ -7,15 +7,15 @@ public class Main11005 {
 		Scanner sc = new Scanner(System.in);
 		long n = sc.nextLong();
 		int b = sc.nextInt();
-		
+
 		StringBuffer buf = new StringBuffer();
 		while (n > 0) {
-            if(n % b >= 10) {
-            	buf.append((char) ((n % b) + 55));
-            } else {
-            	buf.append(n % b);
-            }
-            n /= b;
+			if (n % b >= 10) {
+				buf.append((char) ((n % b) + 55));
+			} else {
+				buf.append(n % b);
+			}
+			n /= b;
 		}
 		System.out.println(buf.reverse().toString());
 		sc.close();
