9cc9c655ed3effed22a13c2aa914d48487a6e41f
songHyeongwon <iks1681@naver.com>
2025-05-15 19:57:07 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step09/Main1181.java

--- BaekJoon/src/step09/Main1181.java
+++ BaekJoon/src/step09/Main1181.java
@@ -9,32 +9,32 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Main1181 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));//識情
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));// 識情
+
 		int n = Integer.parseInt(br.readLine());
 		ArrayList<String> list = new ArrayList<String>();
 		HashSet<String> set = new HashSet<String>();
-		
+
 		for (int i = 0; i < n; i++) {
 			String temp = br.readLine();
-			if(set.add(temp)) {
+			if (set.add(temp)) {
 				list.add(temp);
 			}
 		}
-		list.sort((s1 , s2) -> {
-			if(s1.length() == s2.length()) {
+		list.sort((s1, s2) -> {
+			if (s1.length() == s2.length()) {
 				return s1.compareTo(s2);
 			} else {
 				return s1.length() - s2.length();
 			}
 		});
-		
-		for(String a : list) {
-			bw.write(a+"\n");
+
+		for (String a : list) {
+			bw.write(a + "\n");
 		}
-		
+
 		bw.close();
 		br.close();
 	}
