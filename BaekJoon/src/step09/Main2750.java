35d3da0b3d7fe6470a76e1d32dbb6d2a3ecc7f9e
songHyeongwon <iks1681@naver.com>
2025-05-18 12:33:46 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step09/Main2750.java

--- BaekJoon/src/step09/Main2750.java
+++ BaekJoon/src/step09/Main2750.java
@@ -8,6 +8,7 @@ public class Main2750 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] arr = new int[n];
+		
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
