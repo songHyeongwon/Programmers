45f5cd82114f0f70aa42b404bfeb84e8b7662cb9
songHyeongwon <iks1681@naver.com>
2025-05-20 22:02:22 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step09/Main10814.java

--- BaekJoon/src/step09/Main10814.java
+++ BaekJoon/src/step09/Main10814.java
@@ -11,36 +11,37 @@ import java.util.StringTokenizer;
 public class Main10814 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));//識情
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));// 識情
+
 		int n = Integer.parseInt(br.readLine());
 		ArrayList<User> list = new ArrayList<User>();
-		
+
 		for (int i = 0; i < n; i++) {
 			User user = new User();
-			StringTokenizer token = new StringTokenizer(br.readLine() , " ");
+			StringTokenizer token = new StringTokenizer(br.readLine(), " ");
 			user.idx = i;
 			user.age = Integer.parseInt(token.nextToken());
 			user.name = token.nextToken();
 			list.add(user);
 		}
-		
-		list.sort((u1 , u2) -> {
-			if(u1.age == u2.age) {
+
+		list.sort((u1, u2) -> {
+			if (u1.age == u2.age) {
 				return u1.idx - u2.idx;
 			} else {
 				return u1.age - u2.age;
 			}
 		});
-		
-		for(User a : list) {
-			bw.write(a.age + " " + a.name +"\n");
+
+		for (User a : list) {
+			bw.write(a.age + " " + a.name + "\n");
 		}
-		
+
 		br.close();
 		bw.close();
 	}
 }
+
 class User {
 	int idx;
 	int age;
