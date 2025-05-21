05a1abc3f3a9f909d31536a092c04b50cc7b4ef0
songHyeongwon <iks1681@naver.com>
2025-05-21 20:24:48 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step09/Main10989.java

--- BaekJoon/src/step09/Main10989.java
+++ BaekJoon/src/step09/Main10989.java
@@ -8,12 +8,12 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Main10989 {
-	public static void main(String[] args) throws IOException{
-		
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));//識情
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));// 識情
 		int n = Integer.parseInt(br.readLine());
-		
+
 		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
