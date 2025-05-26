46487232cc4885de0c09972d01a4456cdaf2813e
songHyeongwon <iks1681@naver.com>
2025-05-26 21:21:11 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step10/Main1978.java

--- BaekJoon/src/step10/Main1978.java
+++ BaekJoon/src/step10/Main1978.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main1978 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		int count = 0;
 		for (int i = 0; i < n; i++) {
-			if(getCheck(sc.nextInt())) {
+			if (getCheck(sc.nextInt())) {
 				count++;
 			}
 		}
@@ -18,12 +18,14 @@ public class Main1978 {
 	}
 
 	private static boolean getCheck(int nextInt) {
-		if(nextInt == 1) return false;
-		
+		if (nextInt == 1)
+			return false;
+
 		for (int i = 2; i <= Math.sqrt(nextInt); i++) {
-			if(nextInt % i == 0) return false;
+			if (nextInt % i == 0)
+				return false;
 		}
-		
+
 		return true;
 	}
 }
