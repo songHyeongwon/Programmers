f92f7a346a2a3d9c055b02a337a403d89bb4134d
songHyeongwon <iks1681@naver.com>
2025-05-29 20:37:31 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step10/Main5086.java

--- BaekJoon/src/step10/Main5086.java
+++ BaekJoon/src/step10/Main5086.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Main5086 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while(true) {
+		while (true) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			if(a+b ==0) break;
-			
+			if (a + b == 0)
+				break;
+
 			String result = "neither";
-			if(a % b == 0) {
+			if (a % b == 0) {
 				result = "multiple";
 			}
-			if(b % a == 0) {
+			if (b % a == 0) {
 				result = "factor";
 			}
 			System.out.println(result);
