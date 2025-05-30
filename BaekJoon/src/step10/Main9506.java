f1b16ea4774c6295ec0ffc98ed99aa68c77e945c
songHyeongwon <iks1681@naver.com>
2025-05-30 20:13:29 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step10/Main9506.java

--- BaekJoon/src/step10/Main9506.java
+++ BaekJoon/src/step10/Main9506.java
@@ -9,30 +9,32 @@ public class Main9506 {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			int n = sc.nextInt();
-			if(n == -1) break;
+			if (n == -1)
+				break;
 			getSum(n);
 		}
 		sc.close();
 	}
+
 	public static void getSum(int n) {
 		int temp = 0;
 		List<Integer> list = new ArrayList<>();
 		for (int i = 1; i < n; i++) {
-			if(n % i == 0) {
+			if (n % i == 0) {
 				list.add(i);
 				temp += i;
 			}
 		}
-		if(n == temp) {
-			//6 = 1 + 2 + 3
+		if (n == temp) {
+			// 6 = 1 + 2 + 3
 			System.out.print(String.format("%d = ", n));
-	        StringBuilder result = new StringBuilder();
-	        for (int i = 0; i < list.size(); i++) {
-	            result.append(list.get(i));
-	            if (i < list.size() - 1) {
-	                result.append(" + ");
-	            }
-	        }
+			StringBuilder result = new StringBuilder();
+			for (int i = 0; i < list.size(); i++) {
+				result.append(list.get(i));
+				if (i < list.size() - 1) {
+					result.append(" + ");
+				}
+			}
 			System.out.println(result.toString());
 		} else {
 			System.out.println(String.format("%d is NOT perfect.", n));
