581607613888b4705a2f1851ab0ad1f8eba22065
songHyeongwon <iks1681@naver.com>
2025-06-01 12:52:04 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step11/Main2164.java

--- BaekJoon/src/step11/Main2164.java
+++ BaekJoon/src/step11/Main2164.java
@@ -10,15 +10,15 @@ public class Main2164 {
 		int N = sc.nextInt();
 		sc.close();
 		Queue<Integer> queue = new LinkedList<Integer>();
-		//입력
+		// 입력
 		for (int i = 0; i < N; i++) {
-			queue.add(i+1);
+			queue.add(i + 1);
 		}
-		
-		int result = 0;//반환
-		boolean isCheck = true;//체크
-		while(!queue.isEmpty()) {
-			if(isCheck) {
+
+		int result = 0;// 반환
+		boolean isCheck = true;// 체크
+		while (!queue.isEmpty()) {
+			if (isCheck) {
 				result = queue.poll();
 			} else {
 				queue.add(queue.poll());
