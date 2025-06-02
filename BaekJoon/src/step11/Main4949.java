b1c3610c4c0a0ea98a34c5a4dbcfc2d4994169e5
songHyeongwon <iks1681@naver.com>
2025-06-02 19:33:07 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step11/Main4949.java

--- BaekJoon/src/step11/Main4949.java
+++ BaekJoon/src/step11/Main4949.java
@@ -7,34 +7,37 @@ public class Main4949 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s;
-		
-		while(true) {		
+
+		while (true) {
 			s = sc.nextLine();
-			if(s.equals(".")) break;
+			if (s.equals("."))
+				break;
 			System.out.println(process(s));
 		}
 		sc.close();
 	}
-	
+
 	public static String process(String k) {
 		Stack<Character> stack = new Stack<Character>();
 		for (int i = 0; i < k.length(); i++) {
 			char temp = k.charAt(i);
-			
+
 			switch (temp) {
 			case '(':
-			case '[': 
+			case '[':
 				stack.push(temp);
 				break;
-			case ')': 
-				if(stack.isEmpty() || stack.pop() != '(') return "no";
+			case ')':
+				if (stack.isEmpty() || stack.pop() != '(')
+					return "no";
 				break;
-			case ']': 
-				if(stack.isEmpty() || stack.pop() != '[') return "no";
+			case ']':
+				if (stack.isEmpty() || stack.pop() != '[')
+					return "no";
 				break;
 			}
 		}
-		if(stack.isEmpty()) {
+		if (stack.isEmpty()) {
 			return "yes";
 		} else {
 			return "no";
