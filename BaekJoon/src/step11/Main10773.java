a05e665c90cc81ad149560679cae018d15839564
songHyeongwon <iks1681@naver.com>
2025-06-04 20:10:06 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step11/Main10773.java

--- BaekJoon/src/step11/Main10773.java
+++ BaekJoon/src/step11/Main10773.java
@@ -5,15 +5,15 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main10773 {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		Stack<Integer> stack = new Stack<Integer>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
 		int sum = 0;
 		for (int i = 0; i < N; i++) {
 			int temp = Integer.parseInt(br.readLine());
-			if(temp != 0) {
+			if (temp != 0) {
 				stack.add(temp);
 				sum += temp;
 			} else {
