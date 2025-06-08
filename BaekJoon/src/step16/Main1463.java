374e7699e50d8e1ea648d18b276cd65330a92a54
songHyeongwon <iks1681@naver.com>
2025-06-08 12:41:24 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step16/Main1463.java

--- BaekJoon/src/step16/Main1463.java
+++ BaekJoon/src/step16/Main1463.java
@@ -7,19 +7,21 @@ public class Main1463 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		sc.close();
-		int[] dp = new int[n+1];
+		int[] dp = new int[n + 1];
 		dp[0] = dp[1] = 0;
-		
+
 		for (int i = 2; i < dp.length; i++) {
 			int min;
 			int two = Integer.MAX_VALUE;
 			int three = Integer.MAX_VALUE;
-			
-			if(i % 3 == 0) three = dp[i/3]+1;
-			if(i % 2 == 0) two = dp[i/2]+1;
-			
-			min = dp[i-1] +1;
-			
+
+			if (i % 3 == 0)
+				three = dp[i / 3] + 1;
+			if (i % 2 == 0)
+				two = dp[i / 2] + 1;
+
+			min = dp[i - 1] + 1;
+
 			int result = Math.min(min, Math.min(two, three));
 			dp[i] = result;
 		}
