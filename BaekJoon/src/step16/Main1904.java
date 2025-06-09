bae161be600fba4e3053f703ba8ce625c2828f9a
songHyeongwon <iks1681@naver.com>
2025-06-09 20:11:00 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step16/Main1904.java

--- BaekJoon/src/step16/Main1904.java
+++ BaekJoon/src/step16/Main1904.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main1904 {
 	public static void main(String[] args) {
 		int[] arr = new int[1000001];
-		
+
 		arr[0] = 1;
 		arr[1] = 2;
-		
+
 		for (int i = 2; i < arr.length; i++) {
-			arr[i] = (arr[i-1] + arr[i-2]) % 15746;
+			arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
 		}
 		Scanner sc = new Scanner(System.in);
-		System.out.println(arr[sc.nextInt()-1]);
+		System.out.println(arr[sc.nextInt() - 1]);
 		sc.close();
 	}
 }
