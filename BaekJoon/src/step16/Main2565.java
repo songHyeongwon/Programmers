65a20139244788b4332855044e837b71786974d4
songHyeongwon <iks1681@naver.com>
2025-06-13 20:30:22 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step16/Main2565.java

--- BaekJoon/src/step16/Main2565.java
+++ BaekJoon/src/step16/Main2565.java
@@ -9,29 +9,29 @@ public class Main2565 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int[] arr = new int[N];
-		
+
 		int[] dp = new int[N];
 		int line = 0;
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		
+
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 			map.put(arr[i], sc.nextInt());
 		}
 		Arrays.sort(arr);
-		
+
 		for (int i = 0; i < dp.length; i++) {
 			dp[i] = 1;
 			for (int j = 0; j < i; j++) {
-				if(map.get(arr[i]) > map.get(arr[j])) {
-					dp[i] = Math.max(dp[i], dp[j] +1);
+				if (map.get(arr[i]) > map.get(arr[j])) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
 				}
 			}
 		}
 		for (int i = 0; i < dp.length; i++) {
 			line = Math.max(line, dp[i]);
 		}
-		System.out.println(N-line);
+		System.out.println(N - line);
 		sc.close();
 	}
 }
