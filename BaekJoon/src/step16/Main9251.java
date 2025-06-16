3038d35471c72fca8fa80077670e4d4db3469d07
songHyeongwon <iks1681@naver.com>
2025-06-16 20:27:20 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step16/Main9251.java

--- BaekJoon/src/step16/Main9251.java
+++ BaekJoon/src/step16/Main9251.java
@@ -8,15 +8,15 @@ public class Main9251 {
 		String a = sc.nextLine();
 		String b = sc.nextLine();
 		sc.close();
-		
+
 		int[][] dp = new int[a.length() + 1][b.length() + 1];
-		
+
 		for (int i = 1; i < dp.length; i++) {
 			for (int j = 1; j < dp[i].length; j++) {
-				if(a.charAt(i-1) == b.charAt(j-1)) {
-					dp[i][j] = dp[i-1][j-1] +1;
+				if (a.charAt(i - 1) == b.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
 				} else {
-					dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
 				}
 			}
 		}
