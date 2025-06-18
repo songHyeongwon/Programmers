402d1e28d5fd3fb416a18bf27ff6e54877dd565c
songHyeongwon <iks1681@naver.com>
2025-06-18 19:43:58 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step16/Main10844.java

--- BaekJoon/src/step16/Main10844.java
+++ BaekJoon/src/step16/Main10844.java
@@ -5,31 +5,31 @@ import java.util.Scanner;
 public class Main10844 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int N = sc.nextInt();
-		long[][] dp = new long[N+1][10];
+		long[][] dp = new long[N + 1][10];
 		long count = 0;
 		long mod = 1000000000;
-		
+
 		for (int i = 1; i < 10; i++) {
 			dp[1][i] = 1;
 		}
-		
+
 		for (int i = 2; i <= N; i++) {
 			for (int j = 0; j < dp[i].length; j++) {
-				if(j == 0) {
-					dp[i][j] = dp[i-1][1] % mod;
-				} else if(j == 9) {
-					dp[i][j] = dp[i-1][8] % mod;
+				if (j == 0) {
+					dp[i][j] = dp[i - 1][1] % mod;
+				} else if (j == 9) {
+					dp[i][j] = dp[i - 1][8] % mod;
 				} else {
-					dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
+					dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
 				}
-				if(i == N) {
+				if (i == N) {
 					count += dp[i][j];
 				}
 			}
 		}
-		if(N == 1) {
+		if (N == 1) {
 			System.out.println(9);
 		} else {
 			System.out.println(count % mod);
