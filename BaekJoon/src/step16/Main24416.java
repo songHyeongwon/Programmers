2de4c8f7538cdbb8e09a83171d5d9d502c308703
songHyeongwon <iks1681@naver.com>
2025-07-03 19:53:19 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step16/Main24416.java

--- BaekJoon/src/step16/Main24416.java
+++ BaekJoon/src/step16/Main24416.java
@@ -3,9 +3,10 @@ package step16;
 import java.util.Scanner;
 
 public class Main24416 {
-	
+
 	static int fibCnt = 0;
 	static int fibonacciCnt = 0;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -14,25 +15,27 @@ public class Main24416 {
 		System.out.println(fibCnt + " " + fibonacciCnt);
 		sc.close();
 	}
+
 	public static int fib(int n) {
-		if(n==1 || n ==2) {
+		if (n == 1 || n == 2) {
 			fibCnt++;
 			return 1;
 		} else {
-			return fib(n-1) + fib(n-2);
+			return fib(n - 1) + fib(n - 2);
 		}
 	}
+
 	public static int fib_dp(int n) {
-		int[] dp = new int[n+1];
-		
+		int[] dp = new int[n + 1];
+
 		dp[1] = 1;
 		dp[2] = 1;
-		
-		for(int i=3; i<=n; i++) {
+
+		for (int i = 3; i <= n; i++) {
 			fibonacciCnt++;
-			dp[i] = dp[i-1] + dp[i-2];
+			dp[i] = dp[i - 1] + dp[i - 2];
 		}
-		
+
 		return dp[n];
 	}
 }
