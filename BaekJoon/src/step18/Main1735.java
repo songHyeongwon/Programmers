97f5099efa61e30a88addbd1c5a8bc4569e97bba
songHyeongwon <iks1681@naver.com>
2025-07-04 20:31:45 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step18/Main1735.java

--- BaekJoon/src/step18/Main1735.java
+++ BaekJoon/src/step18/Main1735.java
@@ -12,7 +12,7 @@ public class Main1735 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());
+		int A = Integer.parseInt(st.nextToken()); 
 		int B = Integer.parseInt(st.nextToken());
 
 		st = new StringTokenizer(br.readLine());
