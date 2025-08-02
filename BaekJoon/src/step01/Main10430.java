955361cafb98e1862e385527c3e74f468e25f82a
songHyeongwon <iks1681@naver.com>
2025-08-02 12:36:35 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step01/Main10430.java

--- BaekJoon/src/step01/Main10430.java
+++ BaekJoon/src/step01/Main10430.java
@@ -8,11 +8,11 @@ public class Main10430 {
 		int A = sc.nextInt();
 		int B = sc.nextInt();
 		int C = sc.nextInt();
-		System.out.println((A+B)%C);
-		System.out.println(((A%C) + (B%C))%C); 
-		System.out.println((A*B)%C);
-		System.out.println(((A%C) * (B%C))%C);
+		System.out.println((A + B) % C);
+		System.out.println(((A % C) + (B % C)) % C);
+		System.out.println((A * B) % C);
+		System.out.println(((A % C) * (B % C)) % C);
 		sc.close();
-		
+
 	}
 }
