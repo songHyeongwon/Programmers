ac600af6d0de47bdb8e2002812e2bcc088e1a1ca
songHyeongwon <iks1681@naver.com>
2025-08-05 19:40:15 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step01/Main10998.java

--- BaekJoon/src/step01/Main10998.java
+++ BaekJoon/src/step01/Main10998.java
@@ -7,8 +7,8 @@ public class Main10998 {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		System.out.println(a*b);
-		sc.close(); 
-		
+		System.out.println(a * b);
+		sc.close();
+
 	}
 }
