055a0b5a18cc4f6fa89096f5d1c60edc42fbd630
songHyeongwon <iks1681@naver.com>
2025-08-08 20:09:45 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step02/Main2563.java

--- BaekJoon/src/step02/Main2563.java
+++ BaekJoon/src/step02/Main2563.java
@@ -6,9 +6,9 @@ public class Main2563 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+
 		boolean[][] isCheck = new boolean[100][100];
-		
+
 		for (int i = 0; i < n; i++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
@@ -21,12 +21,13 @@ public class Main2563 {
 		int count = 0;
 		for (int i = 0; i < isCheck.length; i++) {
 			for (int j = 0; j < isCheck[i].length; j++) {
-				if(isCheck[i][j]) count++;
+				if (isCheck[i][j])
+					count++;
 			}
 		}
-		
+
 		System.out.println(count);
 		sc.close();
-		
-	} 
+
+	}
 }
