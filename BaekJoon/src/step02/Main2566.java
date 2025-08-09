45a2a3ecce2006d6cf5c995b686ff40d17694f76
songHyeongwon <iks1681@naver.com>
2025-08-09 12:30:51 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step02/Main2566.java

--- BaekJoon/src/step02/Main2566.java
+++ BaekJoon/src/step02/Main2566.java
@@ -8,20 +8,20 @@ public class Main2566 {
 		int max = Integer.MIN_VALUE;
 		int x = 0;
 		int y = 0;
-		
+
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				int temp = sc.nextInt();
 				max = Math.max(max, temp);
-				if(max == temp) {
-					x = i+1;
-					y = j+1;
+				if (max == temp) {
+					x = i + 1;
+					y = j + 1;
 				}
 			}
 		}
 		sc.close();
-		System.out.println(max); 
+		System.out.println(max);
 		System.out.println(x + " " + y);
-		
+
 	}
 }
