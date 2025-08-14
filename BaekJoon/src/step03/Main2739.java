9641ba3ed7d1c94f9186eb715aecc4231f8dedb3
songHyeongwon <iks1681@naver.com>
2025-08-14 19:45:11 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step03/Main2739.java

--- BaekJoon/src/step03/Main2739.java
+++ BaekJoon/src/step03/Main2739.java
@@ -6,11 +6,11 @@ public class Main2739 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		
-		for(int i = 1; i < 10 ; i++) {
-			System.out.println(a+" * "+i+" = " + a*i);
+
+		for (int i = 1; i < 10; i++) {
+			System.out.println(a + " * " + i + " = " + a * i);
 		}
 		sc.close();
-		
+
 	}
 }
