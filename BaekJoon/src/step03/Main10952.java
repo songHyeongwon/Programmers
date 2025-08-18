461d994e3a1c6781ae74b4202a919dc6ea48f86c
songHyeongwon <iks1681@naver.com>
2025-08-18 20:02:03 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step03/Main10952.java

--- BaekJoon/src/step03/Main10952.java
+++ BaekJoon/src/step03/Main10952.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main10952 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while(true) {
+		while (true) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			if(a == 0 && b == 0) {
+			if (a == 0 && b == 0) {
 				break;
 			}
-			System.out.println(a+b);
+			System.out.println(a + b);
 		}
 		sc.close();
-		
+
 	}
 }
