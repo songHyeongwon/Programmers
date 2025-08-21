97536dd2e10b7a6fff68cc227cf9160de01dbfe9
songHyeongwon <iks1681@naver.com>
2025-08-21 19:48:51 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step03/Main15552.java

--- BaekJoon/src/step03/Main15552.java
+++ BaekJoon/src/step03/Main15552.java
@@ -1,22 +1,21 @@
 package step03;
 
-
 import java.io.*;
 
 public class Main15552 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));//선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));// 선언
 		int t = Integer.parseInt(br.readLine());
-		
-		for(int i = 0 ; i < t ; i++) {
+
+		for (int i = 0; i < t; i++) {
 			String row = br.readLine();
 			String[] rowSplit = row.split(" ");
 			bw.write(Integer.parseInt(rowSplit[0]) + Integer.parseInt(rowSplit[1]) + "\n");
 		}
-		bw.close();//스트림을 닫음
+		bw.close();// 스트림을 닫음
 		br.close();
-		
+
 	}
 }
