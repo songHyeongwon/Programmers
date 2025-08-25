0f46f299d6a35d6ee6d65b55dfb1447f3837b7ad
songHyeongwon <iks1681@naver.com>
2025-08-25 19:44:29 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step04/Main2480.java

--- BaekJoon/src/step04/Main2480.java
+++ BaekJoon/src/step04/Main2480.java
@@ -10,23 +10,23 @@ public class Main2480 {
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for (int i = 0; i < 3; i++) {
 			int temp = sc.nextInt();
-			if(map.containsKey(temp)) {
+			if (map.containsKey(temp)) {
 				map.put(temp, map.get(temp) + 1);
 			} else {
 				map.put(temp, 1);
 			}
 		}
-		if(map.size() == 1) {
+		if (map.size() == 1) {
 			int maxNum = 0;
 			for (Integer k : map.keySet()) {
 				maxNum = Math.max(maxNum, k);
 				break;
 			}
 			result = 10000 + maxNum * 1000;
-		} else if(map.size() == 2) {
+		} else if (map.size() == 2) {
 			int maxNum = 0;
 			for (Integer k : map.keySet()) {
-				if(map.get(k) == 2) {
+				if (map.get(k) == 2) {
 					maxNum = k;
 				}
 			}
@@ -38,9 +38,9 @@ public class Main2480 {
 			}
 			result = maxNum * 100;
 		}
-		
+
 		System.out.println(result);
 		sc.close();
-		
+
 	}
 }
