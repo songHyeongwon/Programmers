625437a69bd64a315d16d207461490256f2eef00
songHyeongwon <iks1681@naver.com>
2025-08-30 11:29:08 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step04/Main14681.java

--- BaekJoon/src/step04/Main14681.java
+++ BaekJoon/src/step04/Main14681.java
@@ -7,22 +7,22 @@ public class Main14681 {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		
-		if(a > 0) {
-			if(b > 0) {
+
+		if (a > 0) {
+			if (b > 0) {
 				System.out.println(1);
 			} else {
 				System.out.println(4);
 			}
 		} else {
-			if(b > 0) {
+			if (b > 0) {
 				System.out.println(2);
 			} else {
 				System.out.println(3);
 			}
 		}
-		
+
 		sc.close();
 	}
-	
+
 }
