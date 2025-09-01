4ac33270e4eaa4d0a8784fd9e46ea5a37ca446a5
songHyeongwon <iks1681@naver.com>
2025-09-01 20:32:16 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step06/Main2562.java

--- BaekJoon/src/step06/Main2562.java
+++ BaekJoon/src/step06/Main2562.java
@@ -7,15 +7,15 @@ public class Main2562 {
 		Scanner sc = new Scanner(System.in);
 		int max = 0;
 		int idx = 0;
-		
-		for(int i = 0; i < 9; i++) {
+
+		for (int i = 0; i < 9; i++) {
 			int temp = sc.nextInt();
-			if(max < temp) {
+			if (max < temp) {
 				max = temp;
-				idx = i+1;
+				idx = i + 1;
 			}
 		}
-		
+
 		System.out.println(max);
 		System.out.println(idx);
 		sc.close();
