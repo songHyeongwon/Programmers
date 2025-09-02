1e764bfd06d52811447a82142a81a7d1fae24de3
songHyeongwon <iks1681@naver.com>
2025-09-02 20:04:44 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step06/Main3052.java

--- BaekJoon/src/step06/Main3052.java
+++ BaekJoon/src/step06/Main3052.java
@@ -7,11 +7,11 @@ public class Main3052 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		HashSet<Integer> set = new HashSet<Integer>();
-		
-		for(int i =0; i <10; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			set.add(sc.nextInt() % 42);
 		}
-		
+
 		System.out.println(set.size());
 		sc.close();
 	}
