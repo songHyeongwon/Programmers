6535542841469c4074f5aa5d22b6ad96412b2830
songHyeongwon <iks1681@naver.com>
2025-09-06 11:06:56 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step06/Main10811.java

--- BaekJoon/src/step06/Main10811.java
+++ BaekJoon/src/step06/Main10811.java
@@ -11,13 +11,13 @@ public class Main10811 {
 		for (int i = 0; i < n; i++) {
 			arr[i] = i + 1;
 		}
-		
+
 		for (int i = 0; i < m; i++) {
 			int k = sc.nextInt();
 			int j = sc.nextInt();
 			arrSort(arr, k, j);
 		}
-		
+
 		StringBuffer buf = new StringBuffer();
 		for (int temp : arr) {
 			buf.append(temp + " ");
@@ -25,19 +25,19 @@ public class Main10811 {
 		System.out.println(buf.toString().trim());
 		sc.close();
 	}
-	
+
 	public static void arrSort(int[] arr, int a, int b) {
-		if(b - a == 0) return;
-		
+		if (b - a == 0)
+			return;
+
 		int[] temp = new int[b - a + 1];
 		int idx = 0;
 		for (int i = a; i <= b; i++) {
-			temp[idx++] = arr[i-1];
+			temp[idx++] = arr[i - 1];
 		}
 		idx = temp.length - 1;
 		for (int i = a; i <= b; i++) {
-			arr[i-1] = temp[idx--];
+			arr[i - 1] = temp[idx--];
 		}
 	}
 }
- 
