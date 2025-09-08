ca28eaf4f3738fd28d7e8a2a0ffd8e79d0efe9f9
songHyeongwon <iks1681@naver.com>
2025-09-08 20:17:14 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step06/Main10818.java

--- BaekJoon/src/step06/Main10818.java
+++ BaekJoon/src/step06/Main10818.java
@@ -8,12 +8,12 @@ public class Main10818 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] arr = new int[n];
-		for(int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
+
 		Arrays.sort(arr);
-		System.out.println(arr[0] + " " + arr[arr.length-1]);
+		System.out.println(arr[0] + " " + arr[arr.length - 1]);
 		sc.close();
 	}
 }
