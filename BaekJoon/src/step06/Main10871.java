674cbb99b0362f98e65db37314aff0ff82ba227e
songHyeongwon <iks1681@naver.com>
2025-09-09 19:46:48 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step06/Main10871.java

--- BaekJoon/src/step06/Main10871.java
+++ BaekJoon/src/step06/Main10871.java
@@ -8,15 +8,16 @@ public class Main10871 {
 		int n = sc.nextInt();
 		int x = sc.nextInt();
 		int[] arr = new int[n];
-		
+
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
+
 		for (int i = 0; i < arr.length; i++) {
-			if(arr[i] < x) System.out.print(arr[i] + " ");
+			if (arr[i] < x)
+				System.out.print(arr[i] + " ");
 		}
-		
+
 		sc.close();
 	}
 }
