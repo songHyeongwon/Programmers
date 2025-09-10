dee74044ab3a85ef40ad44b43352ec2c436f23db
songHyeongwon <iks1681@naver.com>
2025-09-10 19:48:23 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step06/Main10807.java

--- BaekJoon/src/step06/Main10807.java
+++ BaekJoon/src/step06/Main10807.java
@@ -13,10 +13,11 @@ public class Main10807 {
 		int v = sc.nextInt();
 		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
-			if(arr[i] == v) count++;
+			if (arr[i] == v)
+				count++;
 		}
 		System.out.println(count);
 		sc.close();
-		
+
 	}
 }
