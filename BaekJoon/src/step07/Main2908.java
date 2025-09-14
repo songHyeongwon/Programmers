8d661c343db78e1726d156c0e9282d7e0c13b1ac
songHyeongwon <iks1681@naver.com>
2025-09-14 14:37:10 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main2908.java

--- BaekJoon/src/step07/Main2908.java
+++ BaekJoon/src/step07/Main2908.java
@@ -7,25 +7,25 @@ public class Main2908 {
 		Scanner sc = new Scanner(System.in);
 		String a = sc.next();
 		String b = sc.next();
-		
+
 		char[] aArr = new char[a.length()];
 		char[] bArr = new char[b.length()];
 		for (int i = 0; i < aArr.length; i++) {
-			aArr[i] = a.charAt(a.length() -1 - i);
+			aArr[i] = a.charAt(a.length() - 1 - i);
 		}
 		for (int i = 0; i < bArr.length; i++) {
-			bArr[i] = b.charAt(b.length() -1 - i);
+			bArr[i] = b.charAt(b.length() - 1 - i);
 		}
-		
+
 		a = new String(aArr);
 		b = new String(bArr);
-		
-		if(Integer.parseInt(a) > Integer.parseInt(b)) {
+
+		if (Integer.parseInt(a) > Integer.parseInt(b)) {
 			System.out.println(a);
 		} else {
 			System.out.println(b);
 		}
-		
+
 		sc.close();
 	}
 }
