f6f11c9d14723e4dec1682b39b794d68fa17fb93
songHyeongwon <iks1681@naver.com>
2025-09-15 19:45:14 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main5622.java

--- BaekJoon/src/step07/Main5622.java
+++ BaekJoon/src/step07/Main5622.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class Main5622 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		String[] arr = {"" , "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS","TUV","WXYZ"};
-		
+
+		String[] arr = { "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
 		int timeSum = 0;
 		String q = sc.nextLine();
 		int count = 0;
-		while(count != q.length()) {
-			String k = q.charAt(count)+"";
+		while (count != q.length()) {
+			String k = q.charAt(count) + "";
 			for (int i = 0; i < arr.length; i++) {
-				if(arr[i].indexOf(k) > -1) {
-					timeSum += i+2;
+				if (arr[i].indexOf(k) > -1) {
+					timeSum += i + 2;
 					count++;
 					break;
 				}
 			}
-			
+
 		}
-		
+
 		System.out.println(timeSum);
 		sc.close();
 	}
