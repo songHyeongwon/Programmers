f4fa92578b926281db203a9950ab0709644bc12f
songHyeongwon <iks1681@naver.com>
2025-09-17 19:46:59 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main10809.java

--- BaekJoon/src/step07/Main10809.java
+++ BaekJoon/src/step07/Main10809.java
@@ -7,9 +7,9 @@ public class Main10809 {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
 		char k = 'a';
-		
+
 		for (int i = 0; i < 26; i++) {
-			System.out.print(s.indexOf(k+i) + " ");
+			System.out.print(s.indexOf(k + i) + " ");
 		}
 		sc.close();
 	}
