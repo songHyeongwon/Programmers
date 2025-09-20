2801484fc287f16262eb705b69ee2f08ebde390d
songHyeongwon <iks1681@naver.com>
2025-09-20 14:54:28 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main11720.java

--- BaekJoon/src/step07/Main11720.java
+++ BaekJoon/src/step07/Main11720.java
@@ -6,9 +6,9 @@ public class Main11720 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+
 		String temp = sc.next();
-		
+
 		int sum = 0;
 		for (int i = 0; i < n; i++) {
 			sum += Character.getNumericValue(temp.charAt(i));
