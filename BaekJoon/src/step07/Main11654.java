89a471412df3fec64c0d2df9ac30cb6469517fb5
songHyeongwon <iks1681@naver.com>
2025-10-18 10:41:02 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step07/Main11654.java

--- BaekJoon/src/step07/Main11654.java
+++ BaekJoon/src/step07/Main11654.java
@@ -6,7 +6,7 @@ public class Main11654 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String a = sc.next();
-		
+
 		System.out.println(Character.hashCode(a.charAt(0)));
 		sc.close();
 	}
