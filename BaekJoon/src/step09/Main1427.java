d88170ff484af06c52b3627dbe189ddbc8aad4e4
songHyeongwon <iks1681@naver.com>
2025-10-19 14:11:43 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step09/Main1427.java

--- BaekJoon/src/step09/Main1427.java
+++ BaekJoon/src/step09/Main1427.java
@@ -13,7 +13,7 @@ public class Main1427 {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = n.charAt(i);
 		}
-		
+
 		Arrays.sort(arr, Collections.reverseOrder());
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]);
