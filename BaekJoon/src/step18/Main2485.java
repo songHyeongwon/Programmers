04927fcb91f32394ec365b8c5ffc577979b5419f
songHyeongwon <iks1681@naver.com>
2025-11-03 20:02:53 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step18/Main2485.java

--- BaekJoon/src/step18/Main2485.java
+++ BaekJoon/src/step18/Main2485.java
@@ -8,29 +8,30 @@ import java.io.OutputStreamWriter;
 
 public class Main2485 {
 	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-        int[] streetTree =new int[N];
- 
-        for(int i = 0; i < N; i++){
-            streetTree[i]=Integer.parseInt(br.readLine());
-        }
+		int[] streetTree = new int[N];
 
-        br.close();
+		for (int i = 0; i < N; i++) {
+			streetTree[i] = Integer.parseInt(br.readLine());
+		}
 
-        int gcd = 0;
+		br.close();
 
-        for(int i = 0; i < N-1; i++){
-            int distance = streetTree[i+1] - streetTree[i];
-            gcd = getGCD(distance, gcd);
-        }
-        bw.write((streetTree[N-1]-streetTree[0])/gcd+1-(streetTree.length) + "");
-        bw.flush();
-        bw.close();
+		int gcd = 0;
+
+		for (int i = 0; i < N - 1; i++) {
+			int distance = streetTree[i + 1] - streetTree[i];
+			gcd = getGCD(distance, gcd);
+		}
+		bw.write((streetTree[N - 1] - streetTree[0]) / gcd + 1 - (streetTree.length) + "");
+		bw.flush();
+		bw.close();
 	}
+
 	public static int getGCD(int a, int b) {
 		while (b > 0) {
 			int tmp = a;
