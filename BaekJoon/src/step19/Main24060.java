029c9c7b0273e50a8e22906afc9dbabc118ab34d
songHyeongwon <iks1681@naver.com>
2025-11-10 00:03:16 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step19/Main24060.java

--- BaekJoon/src/step19/Main24060.java
+++ BaekJoon/src/step19/Main24060.java
@@ -3,37 +3,40 @@ package step19;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main24060 { 
+public class Main24060 {
 	static int dpCnt = 0;
 	static int K = 0;
 	static int[] arr = null;
 	static int result = -1;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		K = sc.nextInt();
 		arr = new int[n];
-		for (int i = 0; i < n; i++)  {
+		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		merge_sort(0, n-1);
+		merge_sort(0, n - 1);
 		System.out.println(result);
 		sc.close();
 	}
+
 	public static void merge_sort(int p, int r) {
-		if(p < r) {
-			int q = (p + r) /2;
+		if (p < r) {
+			int q = (p + r) / 2;
 			merge_sort(p, q);
-			merge_sort(q+1, r);
+			merge_sort(q + 1, r);
 			merge(p, q, r);
 		}
 	}
+
 	public static void merge(int p, int q, int r) {
 		int i = p;
-		int j = q+1;
+		int j = q + 1;
 		ArrayList<Integer> numList = new ArrayList<>();
 		while (i <= q && j <= r) {
-			if(arr[i] <= arr[j]) {
+			if (arr[i] <= arr[j]) {
 				numList.add(arr[i]);
 				i++;
 			} else {
@@ -41,20 +44,20 @@ public class Main24060 {
 				j++;
 			}
 		}
-		while(i <= q) {
+		while (i <= q) {
 			numList.add(arr[i]);
 			i++;
 		}
-		while(j <= r) {
+		while (j <= r) {
 			numList.add(arr[j]);
 			j++;
 		}
 		i = p;
 		int t = 0;
-		while(i <= r) {
+		while (i <= r) {
 			arr[i] = numList.get(t);
 			dpCnt++;
-			if(dpCnt == K) {
+			if (dpCnt == K) {
 				result = arr[i];
 			}
 			i++;
