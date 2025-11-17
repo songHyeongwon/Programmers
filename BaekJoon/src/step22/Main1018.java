6115fcdfe16bb09de2214bb6a6260b0856b51735
songHyeongwon <iks1681@naver.com>
2025-11-17 19:33:52 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step22/Main1018.java

--- BaekJoon/src/step22/Main1018.java
+++ BaekJoon/src/step22/Main1018.java
@@ -7,22 +7,22 @@ public class Main1018 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		
+
 		boolean[][] arr = new boolean[N][M];
-		
+
 		for (int i = 0; i < N; i++) {
 			String a = sc.next();
 			for (int j = 0; j < M; j++) {
-				if(a.charAt(j) == 'W') {
+				if (a.charAt(j) == 'W') {
 					arr[i][j] = true;
 				}
 			}
 
 		}
 		sc.close();
-		int minCnt = 8*8;
-		for (int i = 0; i <= N-8; i++) {
-			for (int j = 0; j <= M-8; j++) {
+		int minCnt = 8 * 8;
+		for (int i = 0; i <= N - 8; i++) {
+			for (int j = 0; j <= M - 8; j++) {
 				minCnt = Math.min(minCnt, getCount(arr, i, j));
 			}
 		}
@@ -36,12 +36,13 @@ public class Main1018 {
 			temp = !temp;
 			for (int j = 0; j < 8; j++) {
 				temp = !temp;
-				if(i == 0 && j == 0) continue;
-				if(arr[y+i][x+j] != temp) {
+				if (i == 0 && j == 0)
+					continue;
+				if (arr[y + i][x + j] != temp) {
 					result++;
 				}
 			}
 		}
-		return Math.min(result, 64-result);
+		return Math.min(result, 64 - result);
 	}
 }
