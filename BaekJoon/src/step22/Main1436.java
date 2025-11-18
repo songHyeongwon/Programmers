d4029c9a238aba82ed955d3f0dfa16f21d96b565
songHyeongwon <iks1681@naver.com>
2025-11-18 20:29:04 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step22/Main1436.java

--- BaekJoon/src/step22/Main1436.java
+++ BaekJoon/src/step22/Main1436.java
@@ -7,16 +7,16 @@ public class Main1436 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		sc.close();
-		
+
 		int cnt = 0;
 		int endNum = 666;
-		
-		while(cnt != N) {
-			if(String.valueOf(endNum).contains("666")) {
+
+		while (cnt != N) {
+			if (String.valueOf(endNum).contains("666")) {
 				cnt++;
 			}
 			endNum++;
 		}
-		System.out.println(endNum-1);
+		System.out.println(endNum - 1);
 	}
 }
