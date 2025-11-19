8038de2dbf5c49a04caf81b6331315950a6928e4
songHyeongwon <iks1681@naver.com>
2025-11-19 19:39:21 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step22/Main2231.java

--- BaekJoon/src/step22/Main2231.java
+++ BaekJoon/src/step22/Main2231.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main2231 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		int result = 0;
-		
-		for (int i = n-1; i > 0; i--) {
+
+		for (int i = n - 1; i > 0; i--) {
 			int temp = getTemp(i);
-			if(temp == n) {
+			if (temp == n) {
 				result = i;
 			}
 		}
@@ -20,7 +20,7 @@ public class Main2231 {
 	}
 
 	private static int getTemp(int i) {
-		String s = i+"";
+		String s = i + "";
 		for (int j = 0; j < s.length(); j++) {
 			i += Character.getNumericValue(s.charAt(j));
 		}
