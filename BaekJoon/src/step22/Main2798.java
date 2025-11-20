da4a7a57600a59ae9dcf920d171e5a9499789451
songHyeongwon <iks1681@naver.com>
2025-11-20 20:15:52 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step22/Main2798.java

--- BaekJoon/src/step22/Main2798.java
+++ BaekJoon/src/step22/Main2798.java
@@ -5,40 +5,41 @@ import java.util.Scanner;
 public class Main2798 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		int sum = 0;//합계
-		
+		int sum = 0;// 합계
+
 		int[] arr = new int[n];
-		
+
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
-		sum = getSum(arr , n ,m);
-		
+
+		sum = getSum(arr, n, m);
+
 		System.out.println(sum);
-		
+
 		sc.close();
 	}
 
 	static int getSum(int[] arr, int n, int m) {
 		// TODO Auto-generated method stub
 		int result = 0;
-		
-		for (int i = 0; i < n-2; i++) {
-			for (int j = i+1; j < n-1; j++) {
-				for (int k = j+1; k < n; k++) {
+
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				for (int k = j + 1; k < n; k++) {
 					int tempSum = arr[i] + arr[j] + arr[k];
-					if(tempSum == m) return tempSum;
-					if(tempSum < m) {
+					if (tempSum == m)
+						return tempSum;
+					if (tempSum < m) {
 						result = Math.max(result, tempSum);
 					}
 				}
 			}
 		}
-		
+
 		return result;
 	}
 }
