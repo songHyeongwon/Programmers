6f94a9c03efeb2509fe20f1a235de74721be0f60
songHyeongwon <iks1681@naver.com>
2025-11-22 12:24:03 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step22/Main19532.java

--- BaekJoon/src/step22/Main19532.java
+++ BaekJoon/src/step22/Main19532.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main19532 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a,b,c,e,d,f;
+		int a, b, c, e, d, f;
 		a = sc.nextInt();
 		b = sc.nextInt();
 		c = sc.nextInt();
