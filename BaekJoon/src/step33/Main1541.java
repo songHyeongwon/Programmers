4f645198bcde135b5731e15e7ee497341f7dc70a
songHyeongwon <iks1681@naver.com>
2025-11-25 20:49:18 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step33/Main1541.java

--- BaekJoon/src/step33/Main1541.java
+++ BaekJoon/src/step33/Main1541.java
@@ -6,16 +6,16 @@ public class Main1541 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-		
+
 		String[] arr = str.split("-");
-		int sum = 0;//Integer.parseInt(arr[0]);
+		int sum = 0;// Integer.parseInt(arr[0]);
 		for (int i = 0; i < arr.length; i++) {
 			String[] tempArr = arr[i].split("[+]");
 			int tempSum = 0;
 			for (int j = 0; j < tempArr.length; j++) {
 				tempSum += Integer.parseInt(tempArr[j]);
 			}
-			if(i == 0) {
+			if (i == 0) {
 				sum = tempSum;
 			} else {
 				sum -= tempSum;
