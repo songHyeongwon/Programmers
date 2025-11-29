0f491e55ced9433d98f5882ac479d09f6e18e1d0
songHyeongwon <iks1681@naver.com>
2025-11-29 11:00:06 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step33/Main13305.java

--- BaekJoon/src/step33/Main13305.java
+++ BaekJoon/src/step33/Main13305.java
@@ -6,20 +6,20 @@ public class Main13305 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		long[] line = new long[N-1];
+		long[] line = new long[N - 1];
 		long[] city = new long[N];
-		
+
 		for (int i = 0; i < line.length; i++) {
 			line[i] = sc.nextLong();
 		}
 		for (int i = 0; i < city.length; i++) {
 			city[i] = sc.nextLong();
 		}
-		
-		long sum = 0; //합산된 값
+
+		long sum = 0; // 합산된 값
 		long beforCity = city[0];
-		for (int i = 0; i < city.length-1; i++) {
-			if(city[i] < beforCity) {
+		for (int i = 0; i < city.length - 1; i++) {
+			if (city[i] < beforCity) {
 				beforCity = city[i];
 			}
 			sum += (beforCity * line[i]);
