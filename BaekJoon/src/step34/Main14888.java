54ae16e113064c5bb1fedc8fecc52a1ffcf13c95
songHyeongwon <iks1681@naver.com>
2025-12-02 01:42:33 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step34/Main14888.java

--- BaekJoon/src/step34/Main14888.java
+++ BaekJoon/src/step34/Main14888.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main14888 {
 	static int[] numArr = null;
 	static int[] dustks = new int[4];
-	static char[] charArr = new char[]{'+' , '-' , '*' , '/'};
-	
+	static char[] charArr = new char[] { '+', '-', '*', '/' };
+
 	static int max = Integer.MIN_VALUE;
 	static int min = Integer.MAX_VALUE;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		numArr = new int[n];
 		for (int i = 0; i < numArr.length; i++) {
@@ -22,29 +22,29 @@ public class Main14888 {
 			dustks[i] = sc.nextInt();
 		}
 		sc.close();
-		getDfs(1 , numArr[0]);
-		
+		getDfs(1, numArr[0]);
+
 		System.out.println(max);
 		System.out.println(min);
 	}
 
-	private static void getDfs(int dip , int sum) {
-		if(dip == numArr.length) {
+	private static void getDfs(int dip, int sum) {
+		if (dip == numArr.length) {
 			max = Math.max(max, sum);
 			min = Math.min(min, sum);
 			return;
 		}
-		
-		for(int i = 0; i < dustks.length; i++) {
-			if(dustks[i] > 0) {
+
+		for (int i = 0; i < dustks.length; i++) {
+			if (dustks[i] > 0) {
 				dustks[i]--;
-				getDfs(dip + 1 , getSum(charArr[i] ,sum , numArr[dip]));
+				getDfs(dip + 1, getSum(charArr[i], sum, numArr[dip]));
 				dustks[i]++;
 			}
 		}
 	}
-	
-	private static int getSum(char c , int result, int nextNum) {
+
+	private static int getSum(char c, int result, int nextNum) {
 		switch (c) {
 		case '+':
 			result += nextNum;
