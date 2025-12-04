16fbe680e7a99d190825f1b4049571911298e1f7
songHyeongwon <iks1681@naver.com>
2025-12-04 19:52:19 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step34/Main15649.java

--- BaekJoon/src/step34/Main15649.java
+++ BaekJoon/src/step34/Main15649.java
@@ -8,25 +8,26 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main15649 {	
-	public static void main(String[] args) throws IOException{
+public class Main15649 {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));//識情
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));// 識情
+
 		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
 		int n = Integer.parseInt(token.nextToken());
 		int m = Integer.parseInt(token.nextToken());
-		boolean[] isCheck = new boolean[n+1];
-		
-		getOut(n , m , isCheck, bw , new ArrayList<Integer>());
-		
+		boolean[] isCheck = new boolean[n + 1];
+
+		getOut(n, m, isCheck, bw, new ArrayList<Integer>());
+
 		br.close();
 		bw.close();
 	}
 
-	private static void getOut(int n, int m, boolean[] isCheck , BufferedWriter bw , ArrayList<Integer> list) throws IOException{
+	private static void getOut(int n, int m, boolean[] isCheck, BufferedWriter bw, ArrayList<Integer> list)
+			throws IOException {
 		// TODO Auto-generated method stub
-		if(m == 0) {
+		if (m == 0) {
 			for (int i = 0; i < list.size(); i++) {
 				bw.write(list.get(i) + " ");
 			}
@@ -34,12 +35,12 @@ public class Main15649 {
 			return;
 		}
 		for (int i = 1; i < isCheck.length; i++) {
-			if(!isCheck[i]) {
+			if (!isCheck[i]) {
 				isCheck[i] = true;
 				list.add(i);
-				getOut(n, m-1, isCheck, bw ,list);
+				getOut(n, m - 1, isCheck, bw, list);
 				isCheck[i] = false;
-				list.remove(list.size()-1);
+				list.remove(list.size() - 1);
 			}
 		}
 	}
