0331fb6867e593a63e6b3f1c5e31ad0fa8f88fac
songHyeongwon <iks1681@naver.com>
2025-12-05 20:13:45 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step34/Main15650.java

--- BaekJoon/src/step34/Main15650.java
+++ BaekJoon/src/step34/Main15650.java
@@ -8,19 +8,19 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main15650 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
-		
+
 		int n = Integer.parseInt(token.nextToken());
 		int m = Integer.parseInt(token.nextToken());
-		
-		for (int i = 1<<n+1; i >= 1<<n ; i--) {
-			if(Integer.bitCount(i) == m + 1) {
+
+		for (int i = 1 << n + 1; i >= 1 << n; i--) {
+			if (Integer.bitCount(i) == m + 1) {
 				String temp = Integer.toBinaryString(i);
 				for (int j = 1; j < temp.length(); j++) {
-					if(temp.charAt(j) == '1') {
+					if (temp.charAt(j) == '1') {
 						bw.write(j + " ");
 					}
 				}
