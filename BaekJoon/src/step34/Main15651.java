1ff79ce3dd41b439b54c4c3c1e36c1ed7c8e98ef
songHyeongwon <iks1681@naver.com>
2025-12-06 14:32:17 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step34/Main15651.java

--- BaekJoon/src/step34/Main15651.java
+++ BaekJoon/src/step34/Main15651.java
@@ -8,26 +8,26 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main15651 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
-		
+
 		int n = Integer.parseInt(token.nextToken());
 		int m = Integer.parseInt(token.nextToken());
-		getDfs(n , m , bw , "");
-		
+		getDfs(n, m, bw, "");
+
 		br.close();
 		bw.close();
 	}
 
-	private static void getDfs(int n, int m, BufferedWriter bw, String buf) throws IOException{
-		if(m == 0) {
+	private static void getDfs(int n, int m, BufferedWriter bw, String buf) throws IOException {
+		if (m == 0) {
 			bw.write(buf.trim() + "\n");
 			return;
 		}
 		for (int i = 1; i <= n; i++) {
-			getDfs(n, m-1 , bw, buf + " " +i);
+			getDfs(n, m - 1, bw, buf + " " + i);
 		}
 	}
 }
