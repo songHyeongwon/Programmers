cd7abc6e8029e3af55ec41f199e4f9bc53545dd4
songHyeongwon <iks1681@naver.com>
2025-12-07 13:28:27 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step34/Main15652.java

--- BaekJoon/src/step34/Main15652.java
+++ BaekJoon/src/step34/Main15652.java
@@ -8,26 +8,27 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main15652 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
-		
+
 		int n = Integer.parseInt(token.nextToken());
 		int m = Integer.parseInt(token.nextToken());
-		getDfs(n , m , bw , "", 1);
-		
+		getDfs(n, m, bw, "", 1);
+
 		br.close();
 		bw.close();
 	}
-	private static void getDfs(int n, int m, BufferedWriter bw, String buf, int befor) throws IOException{
-		if(m == 0) {
+
+	private static void getDfs(int n, int m, BufferedWriter bw, String buf, int befor) throws IOException {
+		if (m == 0) {
 			bw.write(buf.trim() + "\n");
 			return;
 		}
 		for (int i = befor; i <= n; i++) {
-			if(i >= befor) {
-				getDfs(n, m-1 , bw, buf + " " +i , i);
+			if (i >= befor) {
+				getDfs(n, m - 1, bw, buf + " " + i, i);
 			}
 		}
 	}
