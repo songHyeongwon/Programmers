7e3456ea1838b8ae179db0a270cd4de8c2fa0b4f
songHyeongwon <iks1681@naver.com>
2025-12-12 07:42:06 +0900

    window bat push Programmers C:/Temp/workspace/Programmers/BaekJoon/src/step49/Main11478.java

--- BaekJoon/src/step49/Main11478.java
+++ BaekJoon/src/step49/Main11478.java
@@ -8,9 +8,9 @@ public class Main11478 {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
 		HashSet<String> set = new HashSet<String>();
-		
+
 		for (int i = 1; i <= s.length(); i++) {
-			for(int k = 0; k + i <= s.length(); k++) {
+			for (int k = 0; k + i <= s.length(); k++) {
 				set.add(s.substring(k, k + i));
 			}
 		}
