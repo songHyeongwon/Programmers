9b567382a8861019e91b01f6ae567a8006423b9e
unknown <iks1681@naver.com>
2026-01-16 20:41:08 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main1358.java

--- BaekJoon/src/OUTTER/Main1358.java
+++ BaekJoon/src/OUTTER/Main1358.java
@@ -21,7 +21,7 @@ public class Main1358 {
 			if(fun(sc.nextInt(), sc.nextInt())) count++;
 		}
 		sc.close();
-		System.out.println(count);
+		System.out.println(count); 
 	}
 	
 	public static boolean fun(int x, int y) {
