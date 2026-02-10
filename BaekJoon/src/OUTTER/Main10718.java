7c901fc0ca5d2014fdeca1a2d5ea132990a0ef45
unknown <iks1681@naver.com>
2026-02-10 20:22:23 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main10718.java

--- BaekJoon/src/OUTTER/Main10718.java
+++ BaekJoon/src/OUTTER/Main10718.java
@@ -3,6 +3,6 @@ package OUTTER;
 public class Main10718 {
 	public static void main(String[] args) {
 		System.out.println("강한친구 대한육군");
-		System.out.print("강한친구 대한육군");
+		System.out.print("강한친구 대한육군"); 
 	}
 }
