44a3fefee3009523ce38e8596b32b25696df958c
unknown <iks1681@naver.com>
2026-02-22 14:27:07 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main1010.java

--- BaekJoon/src/OUTTER/Main1010.java
+++ BaekJoon/src/OUTTER/Main1010.java
@@ -6,22 +6,22 @@ public class Main1010 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int NUM = sc.nextInt();
-		
+
 		for (int i = 0; i < NUM; i++) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-			
+
 			long temp = 1;
-			if(N != M) {
+			if (N != M) {
 				int b = 1;
-				while(N > 0) {
-					temp = temp * M / b; // 29 / 1 * 
+				while (N > 0) {
+					temp = temp * M / b; // 29 / 1 *
 					b++;
 					M--;
 					N--;
 				}
 			}
-			System.out.println(temp); 
+			System.out.println(temp);
 		}
 		sc.close();
 	}
