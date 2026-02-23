d8338889e12f29778c610224e01faca176629b92
unknown <iks1681@naver.com>
2026-02-23 20:19:38 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main1011.java

--- BaekJoon/src/OUTTER/Main1011.java
+++ BaekJoon/src/OUTTER/Main1011.java
@@ -6,22 +6,22 @@ public class Main1011 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
+
 		for (int i = 0; i < T; i++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			
-			int fit = y-x; //거리
-			int max = (int)Math.sqrt(fit); //최대속도
-			
-			if(fit == max * max) {
+
+			int fit = y - x; // 거리
+			int max = (int) Math.sqrt(fit); // 최대속도
+
+			if (fit == max * max) {
 				System.out.println(max * 2 - 1);
-			} else if(fit <= max * max + max){
+			} else if (fit <= max * max + max) {
 				System.out.println(max * 2);
 			} else {
-				System.out.println(max * 2 + 1); 
+				System.out.println(max * 2 + 1);
 			}
-			
+
 		}
 		sc.close();
 	}
