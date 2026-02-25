3d8c206c07b8dd7a6a004021bc2c788947c5a2e6
unknown <iks1681@naver.com>
2026-02-26 01:40:01 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main1065.java

--- BaekJoon/src/OUTTER/Main1065.java
+++ BaekJoon/src/OUTTER/Main1065.java
@@ -8,7 +8,7 @@ public class Main1065 {
 		int N = sc.nextInt();
 		int outNum = 0;
 		for (int i = 1; i <= N; i++) {
-			if(isCheck(i)) {
+			if (isCheck(i)) {
 				outNum++;
 			}
 		}
@@ -17,43 +17,43 @@ public class Main1065 {
 	}
 
 	private static boolean isCheck(int num) {
-		String temp = String.valueOf(num);		
-		if(temp.length() < 2) {
+		String temp = String.valueOf(num);
+		if (temp.length() < 2) {
 			return true;
 		}
-		
+
 		for (int i = 0; i <= 9; i++) {
 			boolean result = false;
-			
+
 			for (int j = 1; j < temp.length(); j++) {
-				if((Character.getNumericValue(temp.charAt(j-1)) + i) == Character.getNumericValue(temp.charAt(j))) {
-					result = true; 
-				} else {
-					result = false;
-					break;
-				}
-			}
-			
-			if(result) {
-				//System.out.println(num+" = "+(i)+"의 증가 등차수열");
-				return true;
-			}
-			
-			for (int j = 1; j < temp.length(); j++) {
-				if((Character.getNumericValue(temp.charAt(j-1)) - i) == Character.getNumericValue(temp.charAt(j))) {
+				if ((Character.getNumericValue(temp.charAt(j - 1)) + i) == Character.getNumericValue(temp.charAt(j))) {
 					result = true;
 				} else {
 					result = false;
 					break;
 				}
 			}
-			
-			if(result) {
-				//System.out.println(num+" = "+(i)+"의 감소 등차수열");
+
+			if (result) {
+				// System.out.println(num+" = "+(i)+"의 증가 등차수열");
+				return true;
+			}
+
+			for (int j = 1; j < temp.length(); j++) {
+				if ((Character.getNumericValue(temp.charAt(j - 1)) - i) == Character.getNumericValue(temp.charAt(j))) {
+					result = true;
+				} else {
+					result = false;
+					break;
+				}
+			}
+
+			if (result) {
+				// System.out.println(num+" = "+(i)+"의 감소 등차수열");
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 }
