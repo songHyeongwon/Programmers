804de0a1b8dc65c67d42441f7423e270b3e25e45
unknown <iks1681@naver.com>
2026-02-28 12:42:18 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main1110.java

--- BaekJoon/src/OUTTER/Main1110.java
+++ BaekJoon/src/OUTTER/Main1110.java
@@ -10,17 +10,17 @@ public class Main1110 {
 		int count = 0;
 		while (true) {
 			count++;
-			if(temp.length() < 2) {
+			if (temp.length() < 2) {
 				temp = "0" + temp;
 			}
 			String c = Character.getNumericValue(temp.charAt(0)) + Character.getNumericValue(temp.charAt(1)) + "";
-			temp = temp.charAt(1) + "" + c.charAt(c.length()-1);
+			temp = temp.charAt(1) + "" + c.charAt(c.length() - 1);
 			temp = Integer.parseInt(temp) + "";
-			if(temp.equals(a)) {
+			if (temp.equals(a)) {
 				break;
 			}
 		}
-		System.out.println(count); 
+		System.out.println(count);
 		sc.close();
 	}
 }
