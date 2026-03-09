ff82df05d94944c4ace893c07cb6a6c17d823d1e
unknown <iks1681@naver.com>
2026-03-09 18:52:43 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main1874.java

--- BaekJoon/src/OUTTER/Main1874.java
+++ BaekJoon/src/OUTTER/Main1874.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 public class Main1874 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int N = sc.nextInt();
 		int idx = 1;
 		Stack<Integer> stack = new Stack<Integer>();
@@ -15,32 +15,32 @@ public class Main1874 {
 			arr[i] = sc.nextInt();
 		}
 		sc.close();
-		 
+
 		StringBuffer buf = new StringBuffer();
-		
+
 		for (int i = 0; i < arr.length; i++) {
-			if(stack.contains(arr[i])) {
-				//있다면
-				while(stack.peek() != arr[i]) {
-					//System.out.println("-");
+			if (stack.contains(arr[i])) {
+				// 있다면
+				while (stack.peek() != arr[i]) {
+					// System.out.println("-");
 					buf.append("-\n");
 					stack.pop();
 				}
-				//System.out.println("-");
+				// System.out.println("-");
 				buf.append("-\n");
 				stack.pop();
 			} else {
-				//없다면
-				if(idx > arr[i]) {//인덱스보다 작다면 이미 틀렸다
+				// 없다면
+				if (idx > arr[i]) {// 인덱스보다 작다면 이미 틀렸다
 					System.out.println("NO");
 					return;
 				} else {
-					while(idx <= arr[i]) {
-						//System.out.println("+");
+					while (idx <= arr[i]) {
+						// System.out.println("+");
 						buf.append("+\n");
 						stack.add(idx++);
 					}
-					i--;//입력해줬으니 i를 줄인다.
+					i--;// 입력해줬으니 i를 줄인다.
 				}
 			}
 
