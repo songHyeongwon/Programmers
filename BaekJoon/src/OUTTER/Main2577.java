39988fd0ab3462b1cae514e85e6f5d2f4dbe9606
unknown <iks1681@naver.com>
2026-03-16 19:08:36 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main2577.java

--- BaekJoon/src/OUTTER/Main2577.java
+++ BaekJoon/src/OUTTER/Main2577.java
@@ -6,20 +6,20 @@ public class Main2577 {
 	public static void main(String[] args) {
 		int[] intArr = new int[10];
 		Scanner sc = new Scanner(System.in);
-		
+
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-		
-		long temp = a*b*c;
-		String k = temp+"";
-		
+
+		long temp = a * b * c;
+		String k = temp + "";
+
 		for (int i = 0; i < k.length(); i++) {
 			intArr[Character.getNumericValue(k.charAt(i))]++;
 		}
-		for(int arr : intArr) {
+		for (int arr : intArr) {
 			System.out.println(arr);
-		} 
+		}
 		sc.close();
 	}
 }
