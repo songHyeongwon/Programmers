03b13f40ed0896e1791cf43821364a2fad8ea3bd
unknown <iks1681@naver.com>
2026-03-21 12:39:31 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/step52/Main2941.java

--- BaekJoon/src/step52/Main2941.java
+++ BaekJoon/src/step52/Main2941.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main2941 {
 	public static void main(String[] args) {
-		String[] arr = {"c=" , "c-" , "dz=" , "d-" , "lj" , "nj", "s=" , "z="};
+		String[] arr = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
 		Scanner sc = new Scanner(System.in);
 		String temp = sc.nextLine();
 		for (int i = 0; i < arr.length; i++) {
