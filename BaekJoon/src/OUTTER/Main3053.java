9a273f10bdabd4d46a81d539b2ac081d472396cd
unknown <iks1681@naver.com>
2026-03-28 10:53:00 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main3053.java

--- BaekJoon/src/OUTTER/Main3053.java
+++ BaekJoon/src/OUTTER/Main3053.java
@@ -7,8 +7,8 @@ public class Main3053 {
 		Scanner sc = new Scanner(System.in);
 		int R = sc.nextInt();
 		sc.close();
-		
-		System.out.println(Math.PI * (R*R));
-		System.out.println((double)2 * R * R);
-	} 
+
+		System.out.println(Math.PI * (R * R));
+		System.out.println((double) 2 * R * R);
+	}
 }
