b597b9a592ebe8b85f5b7fc9459771f02f456e57
unknown <iks1681@naver.com>
2026-03-30 18:41:15 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main4153.java

--- BaekJoon/src/OUTTER/Main4153.java
+++ BaekJoon/src/OUTTER/Main4153.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 public class Main4153 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		while (true) {
 			int[] arr = new int[3];
 			int sum = 0;
@@ -15,14 +14,15 @@ public class Main4153 {
 				arr[i] = sc.nextInt();
 				sum += arr[i];
 			}
-			if(sum == 0) break;
+			if (sum == 0)
+				break;
 			Arrays.sort(arr);
-			if(Math.pow(arr[2], 2) == Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) {
+			if (Math.pow(arr[2], 2) == Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) {
 				System.out.println("right");
 			} else {
 				System.out.println("wrong");
 			}
-		} 
+		}
 		sc.close();
 	}
 }
