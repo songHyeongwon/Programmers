edad8b2a74e71cc36102993a7660b502b5f9415d
unknown <iks1681@naver.com>
2026-03-31 19:38:22 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main4344.java

--- BaekJoon/src/OUTTER/Main4344.java
+++ BaekJoon/src/OUTTER/Main4344.java
@@ -7,28 +7,28 @@ public class Main4344 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		double[] avgArr = new double[n];
-		
+
 		for (int i = 0; i < avgArr.length; i++) {
 			int row = sc.nextInt();
 			double[] rowArr = new double[row];
 			double ave = 0;
-			
+
 			for (int j = 0; j < row; j++) {
 				rowArr[j] = sc.nextDouble();
 				ave += rowArr[j];
-			} 
+			}
 			ave = ave / row;
 			int count = 0;
 			for (int j = 0; j < row; j++) {
-				if( ave < rowArr[j]) {
+				if (ave < rowArr[j]) {
 					count++;
 				}
 			}
-			
-			avgArr[i] = ((double)count / (double)row) * 100;
+
+			avgArr[i] = ((double) count / (double) row) * 100;
 		}
 		for (int i = 0; i < avgArr.length; i++) {
-			System.out.println(String.format("%.3f", avgArr[i]) +"%");
+			System.out.println(String.format("%.3f", avgArr[i]) + "%");
 		}
 		sc.close();
 	}
