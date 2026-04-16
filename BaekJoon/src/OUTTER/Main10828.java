5f44847165daf65d5405af499af663ec55269ea2
unknown <iks1681@naver.com>
2026-04-16 19:33:13 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main10828.java

--- BaekJoon/src/OUTTER/Main10828.java
+++ BaekJoon/src/OUTTER/Main10828.java
@@ -6,24 +6,24 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main10828 {
-	
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		Stack<Integer> stack = new Stack<Integer>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer tk = null;
 		StringBuilder sb = new StringBuilder();
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
-			tk = new StringTokenizer(str , " "); 
+			tk = new StringTokenizer(str, " ");
 			switch (tk.nextToken()) {
 			case "push":
 				stack.add(Integer.parseInt(tk.nextToken()));
 				break;
 			case "pop":
-				if(stack.isEmpty()) {
+				if (stack.isEmpty()) {
 					sb.append(-1).append("\n");
 				} else {
 					sb.append(stack.pop()).append("\n");
@@ -33,14 +33,14 @@ public class Main10828 {
 				sb.append(stack.size()).append("\n");
 				break;
 			case "empty":
-				if(stack.isEmpty()) {
+				if (stack.isEmpty()) {
 					sb.append(1).append("\n");
 				} else {
 					sb.append(0).append("\n");
 				}
 				break;
 			case "top":
-				if(stack.isEmpty()) {
+				if (stack.isEmpty()) {
 					sb.append(-1).append("\n");
 				} else {
 					sb.append(stack.peek()).append("\n");
