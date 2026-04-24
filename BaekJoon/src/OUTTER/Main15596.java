e1dfd41f6d12272b6fe5a2cff6aef0173aca59a8
unknown <iks1681@naver.com>
2026-04-24 19:10:26 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main15596.java

--- BaekJoon/src/OUTTER/Main15596.java
+++ BaekJoon/src/OUTTER/Main15596.java
@@ -3,11 +3,11 @@ package OUTTER;
 public class Main15596 {
 	long sum(int[] a) {
 		long temp = 0;
-		
+
 		for (int i = 0; i < a.length; i++) {
 			temp += a[i];
 		}
-		
-		return temp; 
+
+		return temp;
 	};
 }
