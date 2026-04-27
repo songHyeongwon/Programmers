b3f45c0ecb4a5aab538c6f04ca75b0eafec11374
unknown <iks1681@naver.com>
2026-04-27 19:13:10 +0900

    window bat push Programmers C:/Users/iks16/eclipse-workspace/Programmers/BaekJoon/src/OUTTER/Main17478.java

--- BaekJoon/src/OUTTER/Main17478.java
+++ BaekJoon/src/OUTTER/Main17478.java
@@ -16,7 +16,7 @@ public class Main17478 {
 			System.out.println(tap + "라고 답변하였지.");
 			return;
 		}
-		// 0이 되기 전까지는 재귀 반복
+		// 0이 되기 전까지는 재귀 반복 
 		System.out.println(tap + "\"재귀함수가 뭔가요?\"");
 		System.out.println(tap + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
 		System.out.println(tap + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
